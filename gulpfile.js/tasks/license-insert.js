83f5e32c8406b2400a716eba3f431c2f3c8eee0f
dy2288 <Daniel.Yona@gov.bc.ca>
2016-01-28 13:01:57 -0800

    Added license headers to files

--- gulpfile.js/tasks/license-insert.js
+++ gulpfile.js/tasks/license-insert.js
@@ -7,7 +7,7 @@ var name = "First People's Cultural Council";
 var licenseHeader = fs.readFileSync('./license-header-template.txt', 'utf8').replace('{name}', name);
 
 gulp.task('license-insert', function() {
-  return gulp.src(config.sourceAssets + '/*/*.js')
+  return gulp.src(config.sourceAssets + '/**/*.js')
   	.pipe(tap(function(file, t) {
   		if (file.contents.indexOf(licenseHeader) == -1 ) {
 		    file.contents = Buffer.concat([
