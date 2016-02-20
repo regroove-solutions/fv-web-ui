7af3088ac5f45ea0f8e272c298fdbe8ccca1362c
dy2288 <Daniel.Yona@gov.bc.ca>
2016-02-19 17:52:06 -0800

    Improving performance of policy. needs testing

--- NuxeoSecurity/src/main/java/ca/bc/gov/restrictions/LanguageRecorders.java
+++ NuxeoSecurity/src/main/java/ca/bc/gov/restrictions/LanguageRecorders.java
@@ -11,9 +11,6 @@ import org.nuxeo.ecm.core.api.security.ACP;
 import org.nuxeo.ecm.core.api.security.Access;
 import org.nuxeo.ecm.core.api.security.SecurityConstants;
 import org.nuxeo.ecm.core.model.Document;
-import org.nuxeo.ecm.core.query.sql.model.*;
-import org.nuxeo.ecm.core.query.sql.model.SQLQuery.Transformer;
-import org.nuxeo.ecm.core.query.sql.NXQL;
 import org.nuxeo.ecm.core.security.AbstractSecurityPolicy;
 import ca.bc.gov.utils.CustomSecurityConstants;
 
@@ -47,7 +44,17 @@ public class LanguageRecorders extends AbstractSecurityPolicy {
 
     @Override
     public boolean isRestrictingPermission(String permission) {
-        return true;
+
+    	if ( permission.equals(SecurityConstants.ADD_CHILDREN) ||
+    		 permission.equals(SecurityConstants.WRITE) ||
+    		 permission.equals(SecurityConstants.WRITE_PROPERTIES) ||
+    		 permission.equals(CustomSecurityConstants.CAN_ASK_FOR_PUBLISH) ||
+    		 permission.equals(SecurityConstants.REMOVE_CHILDREN) ||
+    		 permission.equals(SecurityConstants.REMOVE)) {
+			return true;
+		}
+
+    	return false;
     }
 
     @Override
@@ -55,20 +62,24 @@ public class LanguageRecorders extends AbstractSecurityPolicy {
             Principal principal, String permission,
             String[] resolvedPermissions, String[] additionalPrincipals) throws SecurityException {
 
-        Access access = Access.UNKNOWN;
         List<String> resolvedPermissionsList = Arrays.asList(resolvedPermissions);
         List<String> additionalPrincipalsList = Arrays.asList(additionalPrincipals);
+
+        // Skip administrators and system
+        if (additionalPrincipalsList.contains("administrators") || principal.equals("system")) {
+            return Access.UNKNOWN;
+        }
+
+        // Skip permissions that are READ, this policy will not limit them
+        if ("BROWSE".equals(permission)) {
+        	return Access.UNKNOWN;
+        }
 
         String docType = doc.getType().getName();
         String docTypeParent = null;
 
         if (doc.getParent() != null) {
         	docTypeParent = doc.getParent().getType().getName();
-        }
-
-        // Skip administrators
-        if (additionalPrincipalsList.contains("administrators")) {
-            return access;
         }
 
         // Permissions apply to recorders only
@@ -99,48 +110,11 @@ public class LanguageRecorders extends AbstractSecurityPolicy {
             }
         }
 
-        return access;
+        return Access.UNKNOWN;
     }
 
     @Override
-    public boolean isExpressibleInQuery(String repositoryName, String queryLanguage) {
+    public boolean isExpressibleInQuery(String repositoryName) {
         return false;
-    }
-
-
-    @Override
-    public SQLQuery.Transformer getQueryTransformer() {
-        return NO_FILE_TRANSFORMER;
-    }
-
-    public static final Transformer NO_FILE_TRANSFORMER = new NoFileTransformer();
-
-    /**
-     * Sample Transformer that adds {@code AND ecm:primaryType <> 'File'} to the query.
-     */
-    public static class NoFileTransformer implements SQLQuery.Transformer {
-
-        /**
-         *
-         */
-        private static final long serialVersionUID = 1L;
-        /** {@code ecm:primaryType <> 'File'} */
-        public static final Predicate NO_FILE = new Predicate(
-                new Reference(NXQL.ECM_PRIMARYTYPE), Operator.NOTEQ, new StringLiteral("File"));
-
-        @Override
-        public SQLQuery transform(Principal principal, SQLQuery query) {
-            WhereClause where = query.where;
-            Predicate predicate;
-            if (where == null || where.predicate == null) {
-                predicate = NO_FILE;
-            } else {
-                // adds an AND ecm:primaryType <> 'File' to the WHERE clause
-                predicate = new Predicate(NO_FILE, Operator.AND, where.predicate);
-            }
-            // return query with updated WHERE clause
-            return new SQLQuery(query.select, query.from, new WhereClause(predicate),
-                    query.groupBy, query.having, query.orderBy, query.limit, query.offset);
-        }
     }
 }
