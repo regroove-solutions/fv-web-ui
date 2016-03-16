a7082c3d06734f70f9cfd068f1f7d2ffcde2d8c6
Remi Cattiau <remi@cattiau.com>
2016-03-16 16:40:53 -0700

    Add operations and actions for dialect publishing

---
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/publisher/operations/UnpublishDialectOperation.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+
+package ca.firstvoices.publisher.operations;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.collectors.DocumentModelCollector;
+import org.nuxeo.ecm.automation.core.collectors.BlobCollector;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.runtime.api.Framework;
+
+import ca.firstvoices.publisher.services.DialectPublisherService;
+
+/**
+ * @author loopingz
+ */
+@Operation(id=UnpublishDialectOperation.ID, category=Constants.CAT_DOCUMENT, label="PublishDialect", description="")
+public class UnpublishDialectOperation {
+
+    public static final String ID = "UnpublishDialect";
+
+    public static final DialectPublisherService service = Framework.getLocalService(DialectPublisherService.class);
+
+    @OperationMethod
+    public void run(DocumentModel input) {
+       service.unpublish(input);
+    }    
+
+}
---
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/publisher/seam/UnpublishDialectActionBean.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+
+package ca.firstvoices.publisher.seam;
+
+import java.io.Serializable;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.StatusMessage;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
+import org.nuxeo.runtime.api.Framework;
+
+import ca.firstvoices.publisher.services.DialectPublisherService;
+
+@Name("unpublishDialectAction")
+@Scope(ScopeType.EVENT)
+public class UnpublishDialectActionBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @In(create = true, required = false)
+    protected transient CoreSession documentManager;
+
+    @In(create = true)
+    protected NavigationContext navigationContext;
+
+    @In(create = true, required = false)
+    protected transient FacesMessages facesMessages;
+
+    // This the method that will be called when the action button/link is
+    // clicked
+    public String doGet() {
+        DialectPublisherService service = Framework.getService(DialectPublisherService.class);
+        if (service == null) {
+            facesMessages.add(StatusMessage.Severity.FATAL, "Can't unpublish the dialect");
+            return null;
+        }
+        service.unpublish(navigationContext.getCurrentDocument());
+        facesMessages.add(StatusMessage.Severity.INFO, "Dialect has been unpublished");
+
+        // stay on the same view
+        return null;
+    }
+
+    public boolean accept() {
+        return navigationContext.getCurrentDocument() != null && 
+                "FVDialect".equals(navigationContext.getCurrentDocument().getDocumentType().getName());
+    }
+
+}
