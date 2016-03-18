d0976c909ba8de2639e842a04bcb5ae45a69a85d
Craig Stuart <Craig.Stuart@gov.bc.ca>
2016-03-18 13:06:10 -0700

    Minor changes by Remi

--- FirstVoicesPublisher/src/main/java/ca/firstvoices/templates/factories/FVDialectFactory.java
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/templates/factories/FVDialectFactory.java
@@ -8,27 +8,22 @@ import org.nuxeo.runtime.api.Framework;
 
 /**
  * Script ignores creation of structured templates within sections
+ * 
  * @author dyona
  */
 public class FVDialectFactory extends SimpleTemplateBasedFactory {
 
-    @Override
-    public void createContentStructure(DocumentModel eventDoc) {
+	@Override
+	public void createContentStructure(DocumentModel eventDoc) {
 
-        PublisherService ps = Framework.getLocalService(PublisherService.class);
-        DocumentModelList sectionRoots = ps.getRootSectionFinder(eventDoc.getCoreSession()).getDefaultSectionRoots(true, true);
+		// Only apply to one type
+		if ("FVDialect".equals(eventDoc.getType())) {
+			if (eventDoc.isProxy()) {
+				return;
+			}
+		}
 
-        // Only apply to one type
-        if ("FVDialect".equals(eventDoc.getType())) {
-            for (DocumentModel sectionRoot : sectionRoots ) {
-                // If trying to create within a publishing area, suppress structured template
-                if (sectionRoot.getPath().isPrefixOf(eventDoc.getPath())) {
-                    return;
-                }
-            }
-        }
-
-        super.createContentStructure(eventDoc);
-    }
+		super.createContentStructure(eventDoc);
+	}
 
 }
