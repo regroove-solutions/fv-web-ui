b337c0acb2043ba6870aa2d81a7efb30f039b016
dy2288 <dy2288@gmail.com>
2016-05-16 09:22:50 -0700

    Placeholder image change

--- app/assets/javascripts/views/pages/explore/archive/dialects.js
+++ app/assets/javascripts/views/pages/explore/archive/dialects.js
@@ -114,7 +114,7 @@ export default class ExploreDialects extends Component {
                             key={tile.uid}
                             title={tile.title}
                             subtitle={tile.description}
-                            ><img src="http://www.firstvoices.com/portal/tag1-1a.jpg" /></GridTile>
+                            ><img src="/assets/images/cover.png" /></GridTile>
                         )}
                     </GridList>
                   </div>
