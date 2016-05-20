5fb0604411cf9eec4654a9b1ce67b697dd5e3fa1
dy2288 <Daniel.Yona@gov.bc.ca>
2016-05-20 15:02:16 -0700

    Fixed bug with publishing changes

--- app/assets/javascripts/providers/FVPortal.js
+++ app/assets/javascripts/providers/FVPortal.js
@@ -5,9 +5,11 @@ import RESTActions from './rest-actions'
 import RESTReducers from './rest-reducers'
 
 const updatePortal = RESTActions.update('FV_PORTAL', 'FVPortal', { headers: { 'X-NXenrichers.document': 'ancestry,portal' } });
+const publishPortal = RESTActions.execute('FV_PORTAL_PUBLISH', 'FVPublish', { headers: { 'X-NXenrichers.document': 'ancestry,portal' } });
+const unpublishPortal = RESTActions.execute('FV_PORTAL_UNPUBLISH', 'FVUnpublishDialect', { headers: { 'X-NXenrichers.document': 'ancestry,portal' } });
 const fetchPortal = RESTActions.fetch('FV_PORTAL', 'FVPortal', { headers: { 'X-NXenrichers.document': 'ancestry,portal' } });
 
-const actions = { fetchPortal, updatePortal };
+const actions = { fetchPortal, updatePortal, publishPortal, unpublishPortal };
 
 const computePortalFactory = RESTReducers.computeFetch('portal');
 
