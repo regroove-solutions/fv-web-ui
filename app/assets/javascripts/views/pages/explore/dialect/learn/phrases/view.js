5872093b40ef4990f3f53707b9f6b9572ce5d5b7
dy2288 <Daniel.Yona@gov.bc.ca>
2016-05-24 17:18:53 -0700

    Photos/video/audio change for phrases

--- app/assets/javascripts/views/pages/explore/dialect/learn/phrases/view.js
+++ app/assets/javascripts/views/pages/explore/dialect/learn/phrases/view.js
@@ -17,8 +17,9 @@ import React, {Component, PropTypes} from 'react';
 import Immutable, { List, Map } from 'immutable';
 import classNames from 'classnames';
 import provide from 'react-redux-provide';
-
 import selectn from 'selectn';
+
+import ConfGlobal from 'conf/local.json';
 
 import ProviderHelpers from 'common/ProviderHelpers';
 
@@ -29,6 +30,8 @@ import PageToolbar from 'views/pages/explore/dialect/page-toolbar';
 import SubViewTranslation from 'views/pages/explore/dialect/learn/base/subview-translation';
 
 import AuthorizationFilter from 'views/components/Document/AuthorizationFilter';
+
+import ImageGallery from 'react-image-gallery';
 
 //import Header from 'views/pages/explore/dialect/header';
 //import PageHeader from 'views/pages/explore/dialect/page-header';
@@ -58,6 +61,8 @@ import Tabs from 'material-ui/lib/tabs/tabs';
 import Tab from 'material-ui/lib/tabs/tab';
 
 import CircularProgress from 'material-ui/lib/circular-progress';
+
+import '!style!css!react-image-gallery/build/image-gallery.css';
 
 /**
 * View phrase entry
@@ -199,6 +204,15 @@ export default class View extends Component {
     const computePhrase = ProviderHelpers.getEntry(this.props.computePhrase, this._getPhrasePath());
     const computeDialect2 = ProviderHelpers.getEntry(this.props.computeDialect2, this.props.routeParams.dialect_path);
 
+    // Generate photos
+
+    let photos = [];
+
+    (selectn('response.contextParameters.phrase.related_pictures', computePhrase) || []).map(function(picture, key) {
+      let image = { original: ConfGlobal.baseURL + picture.path, description: picture['dc:description'] };
+      photos.push(image);
+    })
+
     /**
     * Generate definitions body
     */
@@ -238,6 +252,18 @@ export default class View extends Component {
 
                               <h2>{selectn('response.title', computePhrase)}</h2>
 
+                              <h3>Audio</h3>
+
+                              <div>
+
+                                {(selectn('response.contextParameters.phrase.related_audio.length', computePhrase) === 0) ? <span>No audio is available yet.</span> : ''}
+
+                                {(selectn('response.contextParameters.phrase.related_audio', computePhrase) || []).map(function(audio, key) {
+                                  return <Preview styles={{maxWidth: '350px'}} key={selectn('uid', audio)} expandedValue={audio} type="FVAudio" />;
+                                })}
+
+                              </div>
+
                               <SubViewTranslation group={selectn('response.properties.fv:definitions', computePhrase)} groupByElement="language" groupValue="translation">
                                 <p>Definitions:</p>
                               </SubViewTranslation>
@@ -249,41 +275,35 @@ export default class View extends Component {
                             </div>
 
                             <div className="col-xs-4">
+
+                              {
+                                (photos.length === 0) ?
+                                '' :
+                                
+                                <div>
+                                  <h2>Photos</h2> 
+
+                                  {
+                                    (photos.length === 1) ? 
+                                    <Preview key={selectn('uid', selectn('response.contextParameters.phrase.related_pictures', computePhrase)[0])} expandedValue={selectn('response.contextParameters.phrase.related_pictures', computePhrase)[0]} type="FVPicture" /> :
+                                    <ImageGallery
+                                    ref={i => this._imageGallery = i}
+                                    items={photos}
+                                    slideInterval={2000}
+                                    showThumbnails={false}
+                                    showBullets={true} />
+                                  }
+
+                                </div>
+                              }
+
                               {(selectn('response', computePhrase)) ? <MetadataPanel computeEntity={computePhrase} /> : ''}
                             </div>
 
                           </CardText>
                         </div> 
                       </Tab> 
-                      <Tab label="Photos" id="pictures"> 
-                        <div> 
-                          <CardText>
-                            <h2>Photos</h2> 
-                            <div className="row">
-                              {(selectn('response.contextParameters.phrase.related_pictures', computePhrase) || []).map(function(picture, key) {
-                                return <Preview key={selectn('uid', picture)} expandedValue={picture} type="FVPicture" />;
-                              })}
-
-                              {(selectn('response.contextParameters.phrase.related_pictures.length', computePhrase) === 0) ? <div className="col-xs-12">No photos are available yet.</div> : ''}
-                            </div>
-                          </CardText>
-                        </div>
-                      </Tab> 
-                      <Tab label="Audio" id="audio"> 
-                        <div> 
-                          <CardText>
-                            <h2>Audio</h2> 
-                            <div className="row">
-                              {(selectn('response.contextParameters.phrase.related_audio', computePhrase) || []).map(function(audio, key) {
-                                return <Preview key={selectn('uid', audio)} expandedValue={audio} type="FVAudio" />;
-                              })}
-
-                              {(selectn('response.contextParameters.phrase.related_audio.length', computePhrase) === 0) ? <div className="col-xs-12">No audio is available yet.</div> : ''}
-                            </div>
-                          </CardText>
-                        </div> 
-                      </Tab> 
-                      <Tab label="Video" id="video"> 
+                      <Tab label="Related Videos" id="video"> 
                         <div> 
                           <CardText>
                             <h2>Video</h2> 
