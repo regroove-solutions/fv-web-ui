3c1df6a1fbff35e829636545fbb199d96b0c8e9c
Daniel Yona <Daniel.Yona@gov.bc.ca>
2017-02-07 13:06:43 -0800

    Re-compute alphabet order when alphabet order changes.

--- FirstVoicesPublisher/src/main/java/ca/firstvoices/nativeorder/listeners/ComputeNativeOrderAlphabetListener.java
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/nativeorder/listeners/ComputeNativeOrderAlphabetListener.java
@@ -7,11 +7,13 @@ import java.util.Iterator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.event.DocumentEventTypes;
 import org.nuxeo.ecm.core.api.model.DocumentPart;
 import org.nuxeo.ecm.core.api.model.Property;
 import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.runtime.api.Framework;
@@ -20,9 +22,9 @@ import ca.firstvoices.nativeorder.services.NativeOrderComputeService;
 /**
  * @author dyona
  */
-public class ComputeNativeOrderListener implements EventListener {
+public class ComputeNativeOrderAlphabetListener implements EventListener {
 
-    private static final Log log = LogFactory.getLog(ComputeNativeOrderListener.class);
+    private static final Log log = LogFactory.getLog(ComputeNativeOrderAlphabetListener.class);
 
     protected NativeOrderComputeService service = Framework.getService(NativeOrderComputeService.class);
 
@@ -45,14 +47,12 @@ public class ComputeNativeOrderListener implements EventListener {
         }
 
         // Handle language assets (Words and Phrases)
-        if (doc.getType().equals("FVWord") || doc.getType().equals("FVPhrase")) {
+        if (doc.getType().equals("FVCharacter")) {
 
-            // When modifying, only recompute if dc:title has changed.
-            // If custom order modified, change is via service - so skip.
+            // When modifying, only recompute if order has changed.
             if (event.getName().equals(DocumentEventTypes.BEFORE_DOC_UPDATE)) {
 
-                Boolean titleModified = false;
-                Boolean customOrderModified = false;
+                Boolean orderModified = false;
 
                 DocumentPart[] docParts = doc.getParts();
                 for (DocumentPart docPart : docParts) {
@@ -62,21 +62,21 @@ public class ComputeNativeOrderListener implements EventListener {
                         Property property = dirtyChildrenIterator.next();
                         String propertyName = property.getField().getName().toString();
 
-                        if (propertyName.equals("dc:title") && property.isDirty()) {
-                            titleModified = true;
-                        } else if (propertyName.equals("fv:custom_order") && property.isDirty()) {
-                            customOrderModified = true;
+                        if (propertyName.equals("fvcharacter:alphabet_order") && property.isDirty()) {
+                            orderModified = true;
                         }
                     }
                 }
 
-                if (!titleModified || customOrderModified) {
+                if (!orderModified) {
                     return;
                 }
             }
 
             // Will always run when creating
-            service.computeAssetNativeOrderTranslation(doc);
+            CoreSession session = doc.getCoreSession();
+            DocumentModel dialect = session.getDocument(new IdRef((String) doc.getPropertyValue("fvancestry:dialect")));
+            service.computeDialectNativeOrderTranslation(dialect);
         }
     }
 }
