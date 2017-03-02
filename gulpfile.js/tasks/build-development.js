518a07c5bc7faafdc0bc382ec58fd015b4de3a32
Daniel Yona <Daniel.Yona@gov.bc.ca>
2017-03-02 10:06:45 -0800

    Remove image minify from build (fail on gifsicle)

--- gulpfile.js/tasks/build-development.js
+++ gulpfile.js/tasks/build-development.js
@@ -3,5 +3,5 @@ var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:development', function(cb) {
   //gulpSequence('clean', 'copy', ['fonts', 'images'], ['html'], ['webpack-dev-server'], cb);
-  gulpSequence('clean' , 'copy', 'copy-game-assets' , ['images'], ['html'], ['webpack-dev-server'], cb);
+  gulpSequence('clean' , 'copy', 'copy-game-assets', ['html'], ['webpack-dev-server'], cb);
 });
