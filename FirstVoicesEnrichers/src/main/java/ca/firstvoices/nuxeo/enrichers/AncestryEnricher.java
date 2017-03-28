bfe02982aa0499977e658bf4dcb546852158badd
Daniel Yona <Daniel.Yona@gov.bc.ca>
2017-03-27 18:36:02 -0700

    Account for proxy in ancestry enricher + country/region.

--- FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/enrichers/AncestryEnricher.java
+++ FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/enrichers/AncestryEnricher.java
@@ -9,6 +9,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
 import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
@@ -49,7 +50,7 @@ public class AncestryEnricher extends AbstractJsonEnricher<DocumentModel> {
 		if (languageFamilyId != null && !languageFamilyId.equals("")) {
 
 		    // Allow access to workspace title and uid even for unauthenticated users via Unrestricted subclass
-		    UnrestrictedGetDocumentAncestry ancestryFamilyResolver = new UnrestrictedGetDocumentAncestry(doc.getCoreSession(), languageFamilyId);
+		    UnrestrictedGetDocumentAncestry ancestryFamilyResolver = new UnrestrictedGetDocumentAncestry(doc.getCoreSession(), languageFamilyId, doc.isProxy());
 		    ancestryFamilyResolver.runUnrestricted();
 
 		    if (ancestryFamilyResolver.resolvedDoc != null) {
@@ -67,7 +68,7 @@ public class AncestryEnricher extends AbstractJsonEnricher<DocumentModel> {
 		if (languageId != null && !languageId.equals("")) {
 
 		    // Allow access to workspace title and uid even for unauthenticated users via Unrestricted subclass
-		    UnrestrictedGetDocumentAncestry ancestryLanguageResolver = new UnrestrictedGetDocumentAncestry(doc.getCoreSession(), languageId);
+		    UnrestrictedGetDocumentAncestry ancestryLanguageResolver = new UnrestrictedGetDocumentAncestry(doc.getCoreSession(), languageId, doc.isProxy());
 		    ancestryLanguageResolver.runUnrestricted();
 
 		    if (ancestryLanguageResolver.resolvedDoc != null) {
@@ -85,13 +86,23 @@ public class AncestryEnricher extends AbstractJsonEnricher<DocumentModel> {
 		if (dialectId != null && !dialectId.equals("")) {
 
 		    // Allow access to workspace title and uid even for unauthenticated users via Unrestricted subclass
-		    UnrestrictedGetDocumentAncestry ancestryDialectResolver = new UnrestrictedGetDocumentAncestry(doc.getCoreSession(), dialectId);
+		    UnrestrictedGetDocumentAncestry ancestryDialectResolver = new UnrestrictedGetDocumentAncestry(doc.getCoreSession(), dialectId, doc.isProxy());
 		    ancestryDialectResolver.runUnrestricted();
 
 		    if (ancestryDialectResolver.resolvedDoc != null) {
 		        ObjectNode dialectDoc = mapper.createObjectNode();
 	            dialectDoc.put("uid", dialectId);
 	            dialectDoc.put("dc:title", ancestryDialectResolver.resolvedDoc.getTitle());
+	            dialectDoc.put("path", ancestryDialectResolver.resolvedDoc.getPathAsString());
+
+	            UnrestrictedPropertyValueGetter pvGetter = new UnrestrictedPropertyValueGetter(doc.getCoreSession(), ancestryDialectResolver.resolvedDoc, "fvdialect:country");
+	            pvGetter.runUnrestricted();
+                dialectDoc.put("fvdialect:country", (String) pvGetter.resolvedPropertyValue);
+
+                pvGetter = new UnrestrictedPropertyValueGetter(doc.getCoreSession(), ancestryDialectResolver.resolvedDoc, "fvdialect:region");
+                pvGetter.runUnrestricted();
+	            dialectDoc.put("fvdialect:region", (String) ancestryDialectResolver.resolvedDoc.getPropertyValue("fvdialect:region"));
+
 	            jsonObj.put("dialect", dialectDoc);
 		    }
 		}
@@ -102,17 +113,46 @@ public class AncestryEnricher extends AbstractJsonEnricher<DocumentModel> {
     protected static class UnrestrictedGetDocumentAncestry extends UnrestrictedSessionRunner {
 
         protected final String docId;
+        protected final Boolean isProxy;
         protected DocumentModel resolvedDoc = null;
 
-        protected UnrestrictedGetDocumentAncestry(CoreSession session, String docId) {
+        protected UnrestrictedGetDocumentAncestry(CoreSession session, String docId, Boolean isProxy) {
             super(session);
             this.docId = docId;
+            this.isProxy = isProxy;
         }
 
         @Override
         public void run() {
             IdRef docIdRef = new IdRef(docId);
-            resolvedDoc = session.getDocument(docIdRef);
+
+            if (isProxy) {
+                DocumentModelList proxies = session.getProxies(docIdRef, null);
+
+                if (proxies.size() > 0) {
+                    resolvedDoc = proxies.get(0);
+                }
+            } else {
+                resolvedDoc = session.getDocument(docIdRef);
+            }
+        }
+    }
+
+    protected static class UnrestrictedPropertyValueGetter extends UnrestrictedSessionRunner {
+
+        protected Object resolvedPropertyValue = null;
+        private DocumentModel doc = null;
+        private String field;
+
+        protected UnrestrictedPropertyValueGetter(CoreSession session, DocumentModel doc, String field) {
+            super(session);
+            this.doc = doc;
+            this.field = field;
+        }
+
+        @Override
+        public void run() {
+            resolvedPropertyValue = doc.getPropertyValue(field);
         }
     }
 }
