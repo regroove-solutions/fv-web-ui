ad25f04a983b8adfe36d70c34a14f1f668d7b03f
Daniel Yona <daniel@fpcc.ca>
2018-05-30 22:00:19 -0700

    remove last references to piwik copy task

--- gulpfile.js/tasks/build-development.js
+++ gulpfile.js/tasks/build-development.js
@@ -3,5 +3,5 @@ var gulpSequence = require('gulp-sequence');
 
 gulp.task('build:development', function(cb) {
   //gulpSequence('clean', 'copy', ['fonts', 'images'], ['html'], ['webpack-dev-server'], cb);
-  gulpSequence('clean' , 'copy', 'copy-game-assets', 'copy-piwik-tracker', ['html', 'images'], ['webpack-dev-server'], cb);
+  gulpSequence('clean' , 'copy', 'copy-game-assets', ['html', 'images'], ['webpack-dev-server'], cb);
 });
