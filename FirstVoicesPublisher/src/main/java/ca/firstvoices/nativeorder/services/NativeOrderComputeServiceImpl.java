24c97f862e34e2c41634545c7186e19955de9130
Daniel Yona <daniel@fpcc.ca>
2018-11-09 18:52:46 -0800

    Fix compute native order service and test

--- FirstVoicesPublisher/src/main/java/ca/firstvoices/nativeorder/services/NativeOrderComputeServiceImpl.java
+++ FirstVoicesPublisher/src/main/java/ca/firstvoices/nativeorder/services/NativeOrderComputeServiceImpl.java
@@ -3,12 +3,14 @@
  */
 package ca.firstvoices.nativeorder.services;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 
 import ca.firstvoices.services.AbstractService;
-
 
 /**
  * @author loopingz
@@ -18,76 +20,35 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
 
     private DocumentModel[] loadAlphabet(CoreSession session, DocumentModel dialect) {
         // TODO Know how the Alphabet is ordered
-        DocumentModelList chars = session.query("SELECT * FROM FVCharacter WHERE ecm:ancestorId='"+dialect.getId()+"' ORDER BY fvcharacter:alphabet_order");
+        DocumentModelList chars = session.query("SELECT * FROM FVCharacter WHERE ecm:ancestorId='"+dialect.getId()+"'");
         DocumentModel[] models = new DocumentModel[chars.size()];
-
         models = chars.toArray(models);
+        Arrays.sort(models, new Comparator<DocumentModel>() {
 
-        DocumentModel[] sortedModels = new DocumentModel[chars.size()];
-        int i, j;
-        for( i = 0; i < models.length; i++ ) {
-        	DocumentModel item = models[ i ];
-        	DocumentModel toAdd = item;
-        	String title = (String) item.getPropertyValue( "dc:title" );
-        	Long order = (Long)item.getPropertyValue( "fvcharacter:alphabet_order" );
-        	for( j = i + 1; j < models.length; j++ ) {
-        		DocumentModel item2 = models[ j ];
-        		String title2 = (String) item2.getPropertyValue( "dc:title" );
-        		Long order2 = (Long) item2.getPropertyValue( "fvcharacter:alphabet_order" );
+            @Override
+            public int compare(DocumentModel o1, DocumentModel o2) {
+                String title1 = (String) o1.getPropertyValue("dc:title");
+                String title2 = (String) o2.getPropertyValue("dc:title");
+                if (title1 == title2 && title1 == null) {
+                    return 0;
+                }
+                if (title2 == null) {
+                    return -1;
+                }
+                if (title1 == null) {
+                    return 1;
+                }
+                if (title1.length() < title2.length()) {
+                    return 1;
+                } else if (title1.length() > title2.length()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
 
-        		if( order != null || order2 != null ) {
-        			// use the orders to sort by
-        			if( order == null ) {
-        				// use item2
-        				toAdd = item2;
-        			} else if( order != null && order2 != null ) {
-        				if( order2 < order ) {
-        					// use item2
-        					toAdd = item2;
-        				}
-        			}
-        		} else if( title == null || ( title.length() < title2.length() ) ) {
-        			toAdd = item2;
-        		}
-
-        		title = (String) toAdd.getPropertyValue( "dc:title" );
-        		order = (Long) toAdd.getPropertyValue( "fvcharacter:alphabet_order" );
-        	}
-
-        	sortedModels[ i ] = toAdd;
-        }
-
-        return sortedModels;
-//        try {
-//        Arrays.sort(models, new Comparator<DocumentModel>() {
-//
-//            @Override
-//            public int compare(DocumentModel o1, DocumentModel o2) {
-//                String title1 = (String) o1.getPropertyValue("dc:title");
-//                String title2 = (String) o2.getPropertyValue("dc:title");
-//                if (title1 == title2 && title1 == null) {
-//                    return 0;
-//                }
-//                if (title2 == null) {
-//                    return -1;
-//                }
-//                if (title1 == null) {
-//                    return 1;
-//                }
-//                if (title1.length() < title2.length()) {
-//                    return 1;
-//                } else if (title1.length() > title2.length()) {
-//                    return -1;
-//                } else {
-//                    return 0;
-//                }
-//            }
-//
-//        });
-//        } catch( Exception e ) {
-//        	e.printStackTrace();
-//        }
-//        return models;
+        });
+        return models;
     }
 
     /* (non-Javadoc)
@@ -95,17 +56,11 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
      */
     @Override
     public void computeAssetNativeOrderTranslation(DocumentModel asset) {
-    	// appears that there's a lot of processing going on within the following methods
-    	// last of which, computeNativeOrderTranslation will just return if the asset is immutable
-    	// so, instead of processing all the dialect data, and the alphabet only to do nothing,
-    	// lets check that here
-    	if( !asset.isImmutable() ) {
-    		DocumentModel dialect = getDialect(asset);
-    		CoreSession session = asset.getCoreSession();
-    		// First get the native alphabet
-    		DocumentModel[] chars = loadAlphabet(session, dialect);
-    		computeNativeOrderTranslation(chars, asset);
-    	}
+        DocumentModel dialect = getDialect(asset);
+        CoreSession session = asset.getCoreSession();
+        // First get the native alphabet
+        DocumentModel[] chars = loadAlphabet(session, dialect);
+        computeNativeOrderTranslation(chars, asset);
     }
 
     /* (non-Javadoc)
@@ -122,18 +77,13 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
     }
 
     protected void computeNativeOrderTranslation(DocumentModel[] chars, DocumentModel element) {
-    	if( element.isImmutable() ) {
-    		// We cannot update this element, no point in going any further
-    		return;
-    	}
         String title = (String) element.getPropertyValue("dc:title");
         String nativeTitle = "";
         while (title.length() > 0) {
             boolean found = false;
             for (DocumentModel charDoc : chars) {
                 String charValue = (String) charDoc.getPropertyValue("dc:title");
-                String ucCharValue = (String) charDoc.getPropertyValue("fvcharacter:upper_case_character");
-                if ((charValue != null && title.startsWith(charValue)) || (ucCharValue != null && title.startsWith(ucCharValue))) {
+                if (title.startsWith(charValue)) {
                     nativeTitle += new Character((char) (33 + (Long) charDoc.getPropertyValue("fvcharacter:alphabet_order"))).toString();
                     title = title.substring(charValue.length());
                     found = true;
@@ -149,10 +99,7 @@ public class NativeOrderComputeServiceImpl extends AbstractService implements Na
                 title = title.substring(1);
             }
         }
-
-        if( !element.isImmutable() ) {
-        	element.setPropertyValue("fv:custom_order", nativeTitle);
-        }
+        element.setPropertyValue("fv:custom_order", nativeTitle);
         element.getCoreSession().saveDocument(element);
     }
 
