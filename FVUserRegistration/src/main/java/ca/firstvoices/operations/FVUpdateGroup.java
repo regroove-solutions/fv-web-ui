0f0cdf8479d8eab3c885bee6a0d5a5fd4385664a
Kristof Subryan <vtr_monk@mac.com>
2018-12-07 14:36:56 -0800

    New operations to change user and group attributes.

---
+++ FVUserRegistration/src/main/java/ca/firstvoices/operations/FVUpdateGroup.java
@@ -0,0 +1,130 @@
+package ca.firstvoices.operations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang3.StringUtils;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.automation.core.util.Properties;
+import org.nuxeo.ecm.automation.core.util.StringList;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.usermanager.UserManager;
+
+import static ca.firstvoices.utils.FVRegistrationUtilities.APPEND;
+import static ca.firstvoices.utils.FVRegistrationUtilities.REMOVE;
+import static ca.firstvoices.utils.FVRegistrationUtilities.UPDATE;
+import static ca.firstvoices.utils.FVRegistrationUtilities.updateFVProperty;
+
+@Operation(id = FVUpdateGroup.ID, category = Constants.CAT_USERS_GROUPS, label = "FVUpdateGroup",
+        description = "Updates group information. Possible actions are 'update'(default), 'append' and 'remove' .")
+public class FVUpdateGroup
+{
+    public static final String ID = "FVUpdateGroup";
+
+    public static final String GROUP_SCHEMA = "group";
+
+    protected static final String GROUP_COLON = GROUP_SCHEMA + ':';
+
+    public static final String GROUP_NAME = "groupname";
+
+    public static final String GROUP_LABEL = "grouplabel";
+
+    public static final String GROUP_DESCRIPTION = "description";
+
+    public static final String MEMBERS = "members";
+
+    public static final String SUB_GROUPS = "subGroups";
+
+    public static final String PARENT_GROUPS = "parentGroups";
+
+    @Context
+    protected UserManager userManager;
+
+    @Param(name = "groupname")
+    protected String groupName;
+
+    @Param(name = "grouplabel", required = false)
+    protected String groupLabel;
+
+    @Param(name = "description", required = false)
+    protected String groupDescription;
+
+    @Param(name = "members", required = false)
+    protected StringList members;
+
+    @Param(name = "membersAction", required = false, values = { UPDATE, APPEND,  REMOVE})
+    protected String membersAction = UPDATE;
+
+    @Param(name = "subGroups", required = false)
+    protected StringList subGroups;
+
+    @Param(name = "subGroupsAction", required = false, values = { UPDATE, APPEND,  REMOVE})
+    protected String subGroupsAction = UPDATE;
+
+    @Param(name = "parentGroups", required = false)
+    protected StringList parentGroups;
+
+    @Param(name = "parentGroupsAction", required = false, values = { UPDATE, APPEND,  REMOVE})
+    protected String parentGroupsAction = UPDATE;
+
+
+    @Param(name = "properties", required = false)
+    protected Properties properties = new Properties();
+
+    @OperationMethod
+    public void run() throws OperationException
+    {
+        DocumentModel groupDoc = userManager.getGroupModel(groupName.toLowerCase());
+
+        if (groupDoc == null)
+        {
+                throw new OperationException("Cannot update non-existent group: " + groupName);
+        }
+
+        if( members != null )
+        {
+            updateFVProperty( membersAction, groupDoc, members, GROUP_SCHEMA, MEMBERS );
+        }
+
+        if (subGroups != null)
+        {
+            updateFVProperty( subGroupsAction, groupDoc, subGroups, GROUP_SCHEMA, SUB_GROUPS );
+        }
+
+        if (parentGroups != null )
+        {
+            updateFVProperty( parentGroupsAction, groupDoc, parentGroups, GROUP_SCHEMA, PARENT_GROUPS );
+        }
+
+        for (Entry<String, String> entry : Arrays.asList(
+                new SimpleEntry<>(GROUP_LABEL, groupLabel),
+                new SimpleEntry<>(GROUP_DESCRIPTION, groupDescription)))
+        {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (StringUtils.isNotBlank(value))
+            {
+                properties.put(key, value);
+            }
+        }
+        for (Entry<String, String> entry : properties.entrySet())
+        {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (key.startsWith(GROUP_COLON)) {
+                key = key.substring(GROUP_COLON.length());
+            }
+            groupDoc.setProperty(GROUP_SCHEMA, key, value);
+        }
+
+        userManager.updateGroup(groupDoc);
+        groupDoc = userManager.getGroupModel(groupName.toLowerCase());
+    }
+  }
