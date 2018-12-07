64c899696109a1dc96c4d0d70785a3d83744f605
Kristof Subryan <vtr_monk@mac.com>
2018-12-07 14:39:00 -0800

    Listener to handle "registration validation" event ie. after user configured password.

--- FVUserRegistration/src/main/java/ca/firstvoices/listeners/FVRegistrationCompletionListener.java
+++ FVUserRegistration/src/main/java/ca/firstvoices/listeners/FVRegistrationCompletionListener.java
@@ -1,50 +1,51 @@
 package ca.firstvoices.listeners;
 
+import ca.firstvoices.utils.FVRegistrationUtilities;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.*;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
-import org.nuxeo.ecm.core.work.api.WorkManager;
-import org.nuxeo.runtime.api.Framework;
-
-import java.io.Serializable;
-import java.util.Map;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 
 public class FVRegistrationCompletionListener implements EventListener {
 
-   private static final Log log = LogFactory.getLog(ca.firstvoices.listeners.FVRegistrationCompletionListener.class);
+    private static final Log log = LogFactory.getLog(ca.firstvoices.listeners.FVRegistrationCompletionListener.class);
 
-   // accepts documentRemoved && registrationValidated
-    protected WorkManager workManager = Framework.getService(WorkManager.class);
+    // accepts documentRemoved && registrationValidated
 
     @Override
-    public void handleEvent(Event event)
-    {
+    public void handleEvent(Event event) {
         EventContext ctx;
+        ctx = event.getContext();
+        if (!(ctx instanceof DocumentEventContext)) return;
 
-        switch (event.getName())
-        {
+        DocumentEventContext docCtx = (DocumentEventContext) ctx;
+        DocumentModel registration = docCtx.getSourceDocument();
+
+        FVRegistrationUtilities regUtil = new FVRegistrationUtilities();
+
+        switch (event.getName()) {
             case "documentRemoved":
-                log.info("GOT EVENT documentRemoved");
+                // TODO: use it to make sure user name is not left in the system when registration is deleted on time out
                 break;
+
             case "registrationValidated":
-                ctx = event.getContext();
-                Object[] args = ctx.getArguments();
+                Object[] args = docCtx.getArguments();
+                String principal = docCtx.getCoreSession().getPrincipal().getName();
 
-                for( Object o: args )
-                {
-                    if( o.getClass().getTypeName().equals("FVUserRegistration"))
-                    {
-                        DocumentModel reg = (DocumentModel)o;
+                for (Object o : args) {
+                    if (o == null) break;
 
+                    DocumentModel ureg = (DocumentModel) o;
+                    String oT = ureg.getType();
 
+                    if (oT.equals("FVUserRegistration")) {
+                        regUtil.registrationValidationHandler(ureg);
                     }
                 }
-                log.info("GOT EVENT registrationValidated");
-            break;
-
+                break;
         }
     }
 }
