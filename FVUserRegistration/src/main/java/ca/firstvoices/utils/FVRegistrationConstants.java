eab0a58b9b4d4e6f01d6de69529afcd1353fa5e2
Kristof Subryan <vtr_monk@mac.com>
2018-12-10 18:09:51 -0800

    added constants for registration validation.

--- FVUserRegistration/src/main/java/ca/firstvoices/utils/FVRegistrationConstants.java
+++ FVUserRegistration/src/main/java/ca/firstvoices/utils/FVRegistrationConstants.java
@@ -2,10 +2,17 @@ package ca.firstvoices.utils;
 
 public class FVRegistrationConstants {
     // Registration process ACTIONS constants - NOT timeouts
-    public static final int MID_REGISTRATION_PERIOD_ACT = 1;
-    public static final int REGISTRATION_EXPIRATION_ACT = 2;
-    public static final int REGISTRATION_DELETION_ACT = 3;
-    public static final int NEW_USER_SELF_REGISTRATION_ACT = 5;
+    public static final int MID_REGISTRATION_PERIOD_ACT     = 1;
+    public static final int REGISTRATION_EXPIRATION_ACT     = 2;
+    public static final int REGISTRATION_DELETION_ACT       = 3;
+    public static final int NEW_USER_SELF_REGISTRATION_ACT  = 5;
+
+    // constants for validating registration attempt
+    public static final int REGISTRATION_CAN_PROCEED       = 0;
+    public static final int EMAIL_EXISTS_ERROR             = 1;
+    public static final int LOGIN_EXISTS_ERROR             = 2;
+    public static final int LOGIN_AND_EMAIL_EXIST_ERROR    = 3;
+    public static final int REGISTRATION_EXISTS_ERROR      = 4;
 
     // constants for Group and User update operations
     public static final String APPEND = "append";
@@ -15,7 +22,7 @@ public class FVRegistrationConstants {
 
     // TODO - adjust registration constants here
     // TODO - all values in days
-    public static final int MID_REGISTRATION_PERIOD_IN_DAYS = 4;
-    public static final int REGISTRATION_EXPIRATION_IN_DAYS = 7;
-    public static final int REGISTRATION_DELETION_IN_DAYS = 8;
+    public static final int MID_REGISTRATION_PERIOD_IN_DAYS     = 4;
+    public static final int REGISTRATION_EXPIRATION_IN_DAYS     = 7;
+    public static final int REGISTRATION_DELETION_IN_DAYS       = 8;
 }
