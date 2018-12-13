0ed5b7a041a640ab977211805f3c3501563bdebe
Kristof Subryan <vtr_monk@mac.com>
2018-12-12 16:48:29 -0800

    Listener responds to new event newUserApprovedByLanguageAdministrator which is LAdmin requst to place new user in dialect group

--- FVUserRegistration/src/main/java/ca/firstvoices/listeners/FVRegistrationCompletionListener.java
+++ FVUserRegistration/src/main/java/ca/firstvoices/listeners/FVRegistrationCompletionListener.java
@@ -1,5 +1,6 @@
 package ca.firstvoices.listeners;
 
+import ca.firstvoices.services.FVMoveUserToDialectServiceImpl;
 import ca.firstvoices.utils.FVRegistrationUtilities;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,17 +10,23 @@ import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  */
-public class FVRegistrationCompletionListener implements EventListener {
+public class FVRegistrationCompletionListener implements EventListener  {
 
     private static final Log log = LogFactory.getLog(ca.firstvoices.listeners.FVRegistrationCompletionListener.class);
 
     // accepts documentRemoved && registrationValidated
 
     @Override
-    public void handleEvent(Event event) {
+    public void handleEvent(Event event)
+    {
         EventContext ctx;
         ctx = event.getContext();
         if (!(ctx instanceof DocumentEventContext)) return;
@@ -27,23 +34,47 @@ public class FVRegistrationCompletionListener implements EventListener {
         DocumentEventContext docCtx = (DocumentEventContext) ctx;
 
         FVRegistrationUtilities regUtil = new FVRegistrationUtilities();
+        FVMoveUserToDialectServiceImpl util = new FVMoveUserToDialectServiceImpl();
+        Object[] args;
+        DocumentModel dialect;
 
         switch (event.getName()) {
+            case "newUserApprovedByLanguageAdministrator":
+                dialect = docCtx.getSourceDocument();
+                args = docCtx.getArguments();
+                for( Object o : args )
+                {
+                    if (o == null) break;
+
+                    Map<String, String> cArg = (Map<String, String>)o;
+
+                    try
+                    {
+                        util.placeNewUserInGroup(dialect, cArg.get("groupName"), cArg.get("userName"));
+                    }
+                    catch(Exception e)
+                    {
+                        log.error( e );
+                    }
+                }
+
+                break;
+
             case "documentRemoved":
                 // TODO: use it to make sure user name is not left in the system when registration is deleted on time out
                 break;
 
             case "registrationValidated":
-                Object[] args = docCtx.getArguments();
-                String principal = docCtx.getCoreSession().getPrincipal().getName();
+                args = docCtx.getArguments();
 
-                for (Object o : args) {
+                for (Object o : args)
+                {
                     if (o == null) break;
 
                     DocumentModel ureg = (DocumentModel) o;
-                    String oT = ureg.getType();
+                    String cArg = ureg.getType();
 
-                    if (oT.equals("FVUserRegistration")) {
+                    if (cArg.equals("FVUserRegistration")) {
                         regUtil.registrationValidationHandler( ureg );
                     }
                 }
