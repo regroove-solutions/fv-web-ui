16870690a4e1e59891777c44f447a27b5b2ff220
ksubryan <vtr_monk@mac.com>
2019-01-10 12:20:12 -0800

    Basic Export listener to activate export workers.

--- FVExport/src/main/java/ca/firstvoices/listeners/FVExportListener.java
+++ FVExport/src/main/java/ca/firstvoices/listeners/FVExportListener.java
@@ -1,4 +1,36 @@
 package ca.firstvoices.listeners;
 
-public class FVExportListener {
+import ca.firstvoices.workers.FVCyclicExportWorker;
+import ca.firstvoices.workers.FVExportWorker;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.work.api.WorkManager;
+import org.nuxeo.runtime.api.Framework;
+
+public class FVExportListener
+{
+    private static final Log log = LogFactory.getLog(FVExportListener.class);
+
+    public static final String PRODUCE_FORMATTED_DOCUMENT = "produceFormattedDocument";
+    public static final String AUTO_PRODUCE_FORMATTED_DOCUMENT = "autoProduceFormattedDocument";
+
+    protected WorkManager workManager = Framework.getService(WorkManager.class);
+
+    @Override
+    public void handleEvent(Event event)
+    {
+        switch( event.getName() )
+        {
+            case PRODUCE_FORMATTED_DOCUMENT:
+                FVExportWorker doExportFormattedDoc = new FVExportWorker(); // TODO: pass arguments attached to event
+                workManager.schedule(doExportFormattedDoc, true);
+                break;
+
+            case AUTO_PRODUCE_FORMATTED_DOCUMENT:
+                FVCyclicExportWorker doAutoExportFormattedDoc = new FVCyclicExportWorker(); // TODO: pass arguments attached to event
+                workManager.schedule(doAutoExportFormattedDoc, true);
+                break;
+        }
+    }
 }
