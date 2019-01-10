f9e7ea94855c66cbe779c554b0ae3b061d235d8c
ksubryan <vtr_monk@mac.com>
2019-01-10 12:35:55 -0800

    commented format producers

--- FVExport/src/main/java/ca/firstvoices/format_producers/FV_CSV_Producer.java
+++ FVExport/src/main/java/ca/firstvoices/format_producers/FV_CSV_Producer.java
@@ -1,4 +1,8 @@
 package ca.firstvoices.format_producers;
 
+/*
+    Producer assembling output formatted as CSV.
+*/
+
 public class FV_CSV_Producer {
 }
--- FVExport/src/main/java/ca/firstvoices/format_producers/FV_PDF_Producer.java
+++ FVExport/src/main/java/ca/firstvoices/format_producers/FV_PDF_Producer.java
@@ -1,4 +1,9 @@
 package ca.firstvoices.format_producers;
 
+/*
+    Producer assembling output formatted as PDF.
+*/
+
+
 public class FV_PDF_Producer {
 }
