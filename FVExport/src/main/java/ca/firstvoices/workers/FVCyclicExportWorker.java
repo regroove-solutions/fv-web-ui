713bf2f60e2f06a4de151f3ab8d5772c1ec4c681
ksubryan <vtr_monk@mac.com>
2019-01-10 12:35:11 -0800

    modified export workers

--- FVExport/src/main/java/ca/firstvoices/workers/FVCyclicExportWorker.java
+++ FVExport/src/main/java/ca/firstvoices/workers/FVCyclicExportWorker.java
@@ -1,4 +1,40 @@
 package ca.firstvoices.workers;
 
-public class FVCyclicExportWorker {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.work.AbstractWork;
+
+
+public class FVCyclicExportWorker extends AbstractWork
+{
+    private static final Log log = LogFactory.getLog(FVCyclicExportWorker.class);
+
+    public static final String CYCLIC_EXPORT_WORKER = "cyclicExportWorker";
+
+    @Override
+    public String getCategory() {
+        return CYCLIC_EXPORT_WORKER;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Produce formatted document when triggered by cron.";
+    }
+
+    public FVCyclicExportWorker() {
+        super("cyclic-export-worker");
+    }
+
+    @Override
+    public void work()
+    {
+        try
+        {
+            log.warn("FVCyclicExportWorker is not implemented yet.");
+        }
+        catch (Exception e)
+        {
+            log.warn(e);
+        }
+    }
 }
--- FVExport/src/main/java/ca/firstvoices/workers/FVExportWorker.java
+++ FVExport/src/main/java/ca/firstvoices/workers/FVExportWorker.java
@@ -1,4 +1,39 @@
 package ca.firstvoices.workers;
 
-public class FVExportWorker {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.work.AbstractWork;
+
+
+public class FVExportWorker extends AbstractWork
+{
+    private static final Log log = LogFactory.getLog(FVExportWorker.class);
+
+    public static final String ON_DEMAND_EXPORT_WORKER_ = "demandExportWorker";
+
+    @Override
+    public String getCategory() {
+        return ON_DEMAND_EXPORT_WORKER_;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Produce formatted document when triggered by user.";
+    }
+
+    public FVExportWorker() {
+        super("on-demand-export-worker");
+    }
+
+    @Override
+    public void work()
+    {
+        try
+        {
+            log.warn("FVExportWorker is not implemented yet.");
+        }
+        catch (Exception e) {
+            log.warn(e);
+        }
+    }
 }
