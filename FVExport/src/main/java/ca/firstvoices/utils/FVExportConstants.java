9cfc7619a6e47b1ab5e9b3ee9bd49c634b805cfa
ksubryan <vtr_monk@mac.com>
2019-01-11 13:41:14 -0800

    Export module constants

--- FVExport/src/main/java/ca/firstvoices/utils/FVExportConstants.java
+++ FVExport/src/main/java/ca/firstvoices/utils/FVExportConstants.java
@@ -7,10 +7,19 @@ public class FVExportConstants
     public static final String AUTO_PRODUCE_FORMATTED_DOCUMENT = "autoProduceFormattedDocument";
 
     // Parameters passed in Export trigger events
-    public static final String DIALECT_NAME_EXPORT  = "Dialect";
+    public static final String DIALECT_NAME_EXPORT              = "Dialect";
+    public static final String DIALECT_GUID                     = "D_GUID";
     public static final String QUERY_TO_PREPARE_WORK_FOR_EXPORT = "Query";
-    public static final String INITIATING_PRINCIPAL = "Principal";
-    public static final String EXPORT_FORMAT = "Format";
-    public static final String COLUMNS_TO_EXPORT = "Columns";
+    public static final String INHERITED_FROM_OTHER             = "INHERITED";
+    public static final String INITIATING_PRINCIPAL             = "Principal";
+    public static final String EXPORT_FORMAT                    = "Format";
+    public static final String COLUMNS_TO_EXPORT                = "Columns";
+    public static final String CSV_FORMAT                       = "CSV";
+    public static final String PDF_FORMAT                       = "PDF";
+    public static final String CYCLIC_WORKER_ID                 = "cyclic-export-worker";
+    public static final String CYCLIC_WORKER_QUEUE_ID           = "cyclicExportWorker";
+    public static final String CYCLIC_EXPORT_WORKER_CATEGORY    = CYCLIC_WORKER_QUEUE_ID;
+    public static final String ON_DEMAND_WORKER_QUEUE_ID        = "demandExportWorker";
+    public static final String ON_DEMAND_WORKER_CATEGORY        = ON_DEMAND_WORKER_QUEUE_ID;
 
 }
