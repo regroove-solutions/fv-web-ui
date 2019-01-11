3363f06483647bca33559649369a6e2444cc5507
Chad Oakenfold <web@oakenfold.ca>
2019-01-11 11:35:13 -0800

    Linting

--- app/assets/javascripts/views/hoc/grid-list/with-filter.js
+++ app/assets/javascripts/views/hoc/grid-list/with-filter.js
@@ -1,20 +1,20 @@
-import React, { Component, PropTypes } from "react"
-import Immutable, { List, Map } from "immutable"
-import classNames from "classnames"
-import selectn from "selectn"
+import React, { Component, PropTypes } from 'react'
+import Immutable, { List, Map } from 'immutable'
+import classNames from 'classnames'
+import selectn from 'selectn'
 
-import t from "tcomb-form"
+import t from 'tcomb-form'
 
-import fields from "models/schemas/filter-fields"
-import options from "models/schemas/filter-options"
+import fields from 'models/schemas/filter-fields'
+import options from 'models/schemas/filter-options'
 
-import withToggle from "views/hoc/view/with-toggle"
+import withToggle from 'views/hoc/view/with-toggle'
 
-import ProviderHelpers from "common/ProviderHelpers"
-import FormHelpers from "common/FormHelpers"
+import ProviderHelpers from 'common/ProviderHelpers'
+import FormHelpers from 'common/FormHelpers'
 
-import { RaisedButton, FlatButton, FontIcon } from "material-ui"
-import IntlService from "views/services/intl"
+import { RaisedButton } from 'material-ui'
+import IntlService from 'views/services/intl'
 
 const intl = IntlService.instance
 const FiltersWithToggle = withToggle()
@@ -22,36 +22,36 @@ const FiltersWithToggle = withToggle()
 /**
  * Return 'true' if value was found, 'false' otherwise.
  */
-const defaultFilterFunc = function(propertyToSearch, filterValue) {
+const defaultFilterFunc = function defaultFilterFunc(propertyToSearch, filterValue) {
   if (!propertyToSearch) return true
 
   if (Array.isArray(propertyToSearch)) {
     return !(propertyToSearch.indexOf(filterValue) === -1)
-  } else {
-    return !(propertyToSearch.search(new RegExp(filterValue, "i")) === -1)
   }
-
-  return true
+  return !(propertyToSearch.search(new RegExp(filterValue, 'i')) === -1)
 }
 
 export default function withFilter(ComposedFilter, DefaultFetcherParams) {
   class FilteredGridList extends Component {
     static defaultProps = {
-      filterOptionsKey: "Default",
+      filterOptionsKey: 'Default',
       fullWidth: false,
     }
 
     static propTypes = {
+      applyDefaultFormValues: PropTypes.bool,
+      area: PropTypes.string,
       fetcher: PropTypes.func,
+      fetcherParams: PropTypes.object,
+      filterOptionsKey: PropTypes.string.isRequired,
       fixedList: PropTypes.bool,
       fixedListFetcher: PropTypes.func,
-      fetcherParams: PropTypes.object,
-      initialValues: PropTypes.object,
-      filterOptionsKey: PropTypes.string.isRequired,
-      applyDefaultFormValues: PropTypes.bool,
+      formValues: PropTypes.any, // TODO: set appropriate propType
       fullWidth: PropTypes.bool,
+      initialFormValue: PropTypes.any, // TODO: set appropriate propType
+      initialValues: PropTypes.object,
+      items: PropTypes.any, // TODO: set appropriate propType
       metadata: PropTypes.object,
-      area: PropTypes.string,
     }
 
     constructor(props, context) {
@@ -63,8 +63,7 @@ export default function withFilter(ComposedFilter, DefaultFetcherParams) {
         defaultFormValue: props.formValues,
         initialFormValue: props.initialFormValue,
       }
-
-      ;["_onReset", "_doFilter", "_onFilterSaveForm"].forEach((method) => (this[method] = this[method].bind(this)))
+      ;['_onReset', '_doFilter', '_onFilterSaveForm'].forEach((method) => (this[method] = this[method].bind(this)))
     }
 
     componentDidMount() {
@@ -74,36 +73,107 @@ export default function withFilter(ComposedFilter, DefaultFetcherParams) {
       }
     }
 
-    _doFilter(filters, props = this.props) {
-      // Filter a fixed list (i.e. all items sent to component)
-      if (this.props.fixedList) {
-        let filteredList = new List(props.items).filter(
-          function(item) {
-            // Test each of the filters against item
-            for (let filterKey in filters) {
-              let filterOptions = selectn([props.filterOptionsKey, "fields", filterKey], options)
-              let filterFunc =
-                filterOptions && filterOptions.hasOwnProperty("filterFunc")
-                  ? filterOptions.filterFunc
-                  : defaultFilterFunc
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.area !== this.props.area) {
+        this._onReset(null, nextProps)
+      }
 
-              let filterValue = filters[filterKey]
-              let propertyToSearch = selectn(filterKey, item)
+      // Items may change in a fixed list (e.g. deleted, added)
+      const nextPropsItemsList = Immutable.fromJS(nextProps.items)
+      const prevPropsItemsList = Immutable.fromJS(this.props.items)
 
-              if (!filterFunc(propertyToSearch, filterValue)) {
-                return false
-              }
+      if (this.props.fixedList && nextPropsItemsList.equals(prevPropsItemsList) === false) {
+        this._onReset(null, nextProps)
+      }
+    }
+
+    render() {
+      // TODO: options already declared by import, see about fixing this
+      let options = Map(selectn(this.props.filterOptionsKey, this.state.options)) // eslint-disable-line
+
+      // Replace proxied properties where necessary
+      if (this.props.area) {
+        // TODO: fields already declared by import, see about fixing this
+        // eslint-disable-next-line
+        const fields = Map(options.get("fields")).map(
+          function fieldsMap(field) {
+            if (field.hasOwnProperty('nxql')) {
+              field.nxql = ProviderHelpers.replaceAllWorkspaceSectionKeys(field.nxql, this.props.area)
             }
 
-            return true
+            return field
           }.bind(this)
         )
 
-        props.fixedListFetcher(filteredList)
+        options = options.set('fields', fields)
       }
-      // Filter a remote list (i.e. partial list sent to component)
-      else {
-        let preparedFilters = FormHelpers.prepareFilters(filters, options, this.props.filterOptionsKey)
+
+      return (
+        <div>
+          <div className="row">
+            <div
+              className={classNames('col-xs-12', {
+                'col-md-3': !this.props.fullWidth,
+                'col-md-12': this.props.fullWidth,
+              })}
+            >
+              <form onSubmit={this._onFilterSaveForm}>
+                <FiltersWithToggle
+                  label={intl.trans('views.pages.search.filter_items', 'Filter items', 'first')}
+                  mobileOnly
+                >
+                  <t.form.Form
+                    ref="filter_form"
+                    type={t.struct(selectn(this.props.filterOptionsKey, fields))}
+                    context={this.props.initialValues}
+                    value={this.state.formValue}
+                    options={options.toJS()}
+                  />
+                  <RaisedButton onTouchTap={this._onReset} label={intl.trans('reset', 'Reset', 'first')} primary />{' '}
+                  &nbsp;
+                  <RaisedButton type="submit" label={intl.trans('filter', 'Filter', 'first')} primary />
+                </FiltersWithToggle>
+              </form>
+            </div>
+
+            <div
+              className={classNames('col-xs-12', {
+                'col-md-9': !this.props.fullWidth,
+                'col-md-12': this.props.fullWidth,
+              })}
+            >
+              <ComposedFilter {...this.state} {...this.props} />
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    _doFilter(filters, props = this.props) {
+      // Filter a fixed list (i.e. all items sent to component)
+      if (this.props.fixedList) {
+        const filteredList = new List(props.items).filter(function filteredListFilterer(item) {
+          // Test each of the filters against item
+          for (const filterKey in filters) {
+            const filterOptions = selectn([props.filterOptionsKey, 'fields', filterKey], options)
+            const filterFunc =
+              filterOptions && filterOptions.hasOwnProperty('filterFunc') ? filterOptions.filterFunc : defaultFilterFunc
+
+            const filterValue = filters[filterKey]
+            const propertyToSearch = selectn(filterKey, item)
+
+            if (!filterFunc(propertyToSearch, filterValue)) {
+              return false
+            }
+          }
+
+          return true
+        })
+
+        props.fixedListFetcher(filteredList)
+      } else {
+        // Filter a remote list (i.e. partial list sent to component)
+        const preparedFilters = FormHelpers.prepareFilters(filters, options, this.props.filterOptionsKey)
 
         this.props.fetcher(
           Object.assign({}, this.props.fetcherParams, {
@@ -115,15 +185,16 @@ export default function withFilter(ComposedFilter, DefaultFetcherParams) {
     }
 
     _onReset(event, props = this.props) {
-      let form = this.refs["filter_form"]
+      const form = this.refs.filter_form
 
       // Reset all controlled inputs
-      let inputs = Object.assign({}, selectn("refs.input.refs", form), selectn("inputRef.childRefs", form))
+      const inputs = Object.assign({}, selectn('refs.input.refs', form), selectn('inputRef.childRefs', form))
 
-      let properties = FormHelpers.getProperties(form)
+      // TODO: see if getProperties() is modifying form
+      const properties = FormHelpers.getProperties(form) // eslint-disable-line
 
-      for (let inputKey in inputs) {
-        if (typeof inputs[inputKey].forceReset === "function") {
+      for (const inputKey in inputs) {
+        if (typeof inputs[inputKey].forceReset === 'function') {
           inputs[inputKey].forceReset()
         }
       }
@@ -143,28 +214,14 @@ export default function withFilter(ComposedFilter, DefaultFetcherParams) {
       }
     }
 
-    componentWillReceiveProps(nextProps) {
-      if (nextProps.area != this.props.area) {
-        this._onReset(null, nextProps)
-      }
-
-      // Items may change in a fixed list (e.g. deleted, added)
-      let nextPropsItemsList = Immutable.fromJS(nextProps.items)
-      let prevPropsItemsList = Immutable.fromJS(this.props.items)
-
-      if (this.props.fixedList && nextPropsItemsList.equals(prevPropsItemsList) === false) {
-        this._onReset(null, nextProps)
-      }
-    }
-
     _onFilterSaveForm(e) {
       // Prevent default behaviour
       if (e) {
         e.preventDefault()
       }
 
-      let form = this.refs["filter_form"]
-      let properties = FormHelpers.getProperties(form)
+      const form = this.refs.filter_form
+      const properties = FormHelpers.getProperties(form)
 
       if (Object.keys(properties).length != 0) {
         this.setState({
@@ -175,69 +232,6 @@ export default function withFilter(ComposedFilter, DefaultFetcherParams) {
       } else {
         this._onReset(null)
       }
-    }
-
-    render() {
-      let options = Map(selectn(this.props.filterOptionsKey, this.state.options))
-
-      // Replace proxied properties where necessary
-      if (this.props.area) {
-        let fields = Map(options.get("fields")).map(
-          function(field) {
-            if (field.hasOwnProperty("nxql")) {
-              field.nxql = ProviderHelpers.replaceAllWorkspaceSectionKeys(field.nxql, this.props.area)
-            }
-
-            return field
-          }.bind(this)
-        )
-
-        options = options.set("fields", fields)
-      }
-
-      return (
-        <div>
-          <div className="row">
-            <div
-              className={classNames("col-xs-12", {
-                "col-md-3": !this.props.fullWidth,
-                "col-md-12": this.props.fullWidth,
-              })}
-            >
-              <form onSubmit={this._onFilterSaveForm}>
-                <FiltersWithToggle
-                  label={intl.trans("views.pages.search.filter_items", "Filter items", "first")}
-                  mobileOnly={true}
-                >
-                  <t.form.Form
-                    ref="filter_form"
-                    type={t.struct(selectn(this.props.filterOptionsKey, fields))}
-                    context={this.props.initialValues}
-                    value={this.state.formValue}
-                    options={options.toJS()}
-                  />
-                  <RaisedButton
-                    onTouchTap={this._onReset}
-                    label={intl.trans("reset", "Reset", "first")}
-                    primary={true}
-                  />{" "}
-                  &nbsp;
-                  <RaisedButton type="submit" label={intl.trans("filter", "Filter", "first")} primary={true} />
-                </FiltersWithToggle>
-              </form>
-            </div>
-
-            <div
-              className={classNames("col-xs-12", {
-                "col-md-9": !this.props.fullWidth,
-                "col-md-12": this.props.fullWidth,
-              })}
-            >
-              <ComposedFilter {...this.state} {...this.props} />
-            </div>
-          </div>
-        </div>
-      )
     }
   }
 
