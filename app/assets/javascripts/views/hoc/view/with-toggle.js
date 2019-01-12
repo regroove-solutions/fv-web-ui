7c7ebb8e542e2949dc4d6245d1b3b711e9d7ea1b
Chad Oakenfold <web@oakenfold.ca>
2019-01-11 16:08:46 -0800

    Linting, prettier-ed

--- app/assets/javascripts/views/hoc/view/with-toggle.js
+++ app/assets/javascripts/views/hoc/view/with-toggle.js
@@ -1,39 +1,40 @@
-import React, { Component, PropTypes } from 'react';
-import Immutable, { List, Map } from 'immutable';
-import classNames from 'classnames';
-import IntlService from 'views/services/intl';
+import React, { Component, PropTypes } from 'react'
+import classNames from 'classnames'
+import IntlService from 'views/services/intl'
 
-const intl = IntlService.instance;
-import { RaisedButton, FlatButton, FontIcon } from 'material-ui';
+const intl = IntlService.instance
+import { FlatButton, FontIcon } from 'material-ui'
 
 export default function withToggle() {
   class ViewwithToggle extends Component {
     static defaultProps = {
       mobileOnly: false,
       label: 'Toggle Panel',
-      className: ''
-    };
+      className: '',
+    }
 
     static propTypes = {
       mobileOnly: PropTypes.bool,
-      label: PropTypes.string
-    };
-
-    constructor(props, context) {
-      super(props, context);
-      this.rootClassNames.push(this.props.className);
-      this.state = {
-        open: false
-      };
+      label: PropTypes.string,
+      className: PropTypes.string,
+      children: PropTypes.any, // TODO: set to correct proptype
     }
 
-    rootClassNames = ['panel', 'panel-default'];
+    constructor(props, context) {
+      super(props, context)
+      this.rootClassNames.push(this.props.className)
+      this.state = {
+        open: false,
+      }
+    }
+
+    rootClassNames = ['panel', 'panel-default']
 
     render() {
-      let { mobileOnly, label } = this.props;
+      const { mobileOnly, label } = this.props
 
-      const icon = <FontIcon className="material-icons">{this.state.open ? 'expand_less' : 'expand_more'}</FontIcon>;
-      const labelText = this.state.open ? intl.trans('hide', 'Hide', 'first') : intl.trans('show', 'Show', 'first');
+      const icon = <FontIcon className="material-icons">{this.state.open ? 'expand_less' : 'expand_more'}</FontIcon>
+      const labelText = this.state.open ? intl.trans('hide', 'Hide', 'first') : intl.trans('show', 'Show', 'first')
       return (
         <div className={classNames(...this.rootClassNames)}>
           <div className="panel-heading">
@@ -44,8 +45,8 @@ export default function withToggle() {
               label={labelText}
               labelPosition="before"
               onTouchTap={(e) => {
-                this.setState({ open: !this.state.open });
-                e.preventDefault();
+                this.setState({ open: !this.state.open })
+                e.preventDefault()
               }}
               style={{ float: 'right', lineHeight: 1 }}
             />
@@ -55,9 +56,9 @@ export default function withToggle() {
             {this.props.children}
           </div>
         </div>
-      );
+      )
     }
   }
 
-  return ViewwithToggle;
+  return ViewwithToggle
 }
