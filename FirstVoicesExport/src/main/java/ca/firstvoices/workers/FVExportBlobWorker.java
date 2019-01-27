aa5222f4367d22d91d214a2f192bad66a3aa5133
ksubryan <vtr_monk@mac.com>
2019-01-26 18:53:36 -0800

    removed findWrapper method since it is not needed.

--- FirstVoicesExport/src/main/java/ca/firstvoices/workers/FVExportBlobWorker.java
+++ FirstVoicesExport/src/main/java/ca/firstvoices/workers/FVExportBlobWorker.java
@@ -1,6 +1,5 @@
 package ca.firstvoices.workers;
 
-import ca.firstvoices.utils.FVBlobRelocatorAccessor;
 import ca.firstvoices.utils.FVExportWorkInfo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,13 +8,10 @@ import org.nuxeo.ecm.core.api.*;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.runtime.api.Framework;
 
-import javax.print.Doc;
 import javax.security.auth.login.LoginContext;
 import java.io.*;
-import java.util.HashMap;
 
 import static ca.firstvoices.utils.FVExportConstants.*;
-import static ca.firstvoices.utils.FVExportUtils.*;
 
 public class FVExportBlobWorker extends FVAbstractExportWork
 {
@@ -50,8 +46,7 @@ public class FVExportBlobWorker extends FVAbstractExportWork
             File file = new File(workInfo.filePath);
             FileBlob fileBlob = new FileBlob(file, "text/csv", "UTF-8" );
 
-            // check if wrapper already exists
-            DocumentModel wrapper =  workInfo.wrapper; // findWrapper( session );
+            DocumentModel wrapper =  workInfo.wrapper;
 
             wrapper.setPropertyValue( "file:content", fileBlob );
             wrapper.setPropertyValue( "fvexport:progress",  "FINISHED" );
@@ -66,20 +61,5 @@ public class FVExportBlobWorker extends FVAbstractExportWork
         {
             e.printStackTrace();
         }
-    }
-
-    private DocumentModel findWrapper( CoreSession session)
-    {
-        DocumentModel wrapper = null;
-
-        String wrapperQ = "SELECT * FROM FVExport WHERE ecm:ancestorId = '" + workInfo.resourcesFolderGUID + "' AND fvexport:workdigest = '" + workInfo.workDigest + "' AND fvexport:exportdigest = '" + workInfo.exportDigest + "'";
-        DocumentModelList docs = session.query( wrapperQ );
-
-        if( docs != null && docs.size() > 0)
-        {
-            wrapper = docs.get( 0 );
-        }
-
-        return wrapper;
     }
 }
