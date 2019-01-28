66b7fad1c97b4ccd56bf690d6d0d8bfe10030721
ksubryan <vtr_monk@mac.com>
2019-01-27 18:44:32 -0800

    changing compund info to be stored as an array

--- FirstVoicesExport/src/main/java/ca/firstvoices/utils/ExportColumnRecord.java
+++ FirstVoicesExport/src/main/java/ca/firstvoices/utils/ExportColumnRecord.java
@@ -1,19 +1,20 @@
 package ca.firstvoices.utils;
 
+import ca.firstvoices.property_readers.FV_PropertyValueWithColumnName;
 import org.nuxeo.ecm.automation.core.util.StringList;
 
 import java.util.Map;
 
 public class ExportColumnRecord
 {
-    public String               colID;          // column label as received from UI to identify property
-    public String               property;       // property string to retrieve value
-    public Boolean              useForExport;   // set to true if it is ready to be used for export
-    public Integer              numCols;        // max number of columns we want to allow in csv
-    public Class                requiredPropertyReader;
-    public Map<String, String>  compound;
+    public String                           colID;          // column label as received from UI to identify property
+    public String                           property;       // property string to retrieve value
+    public Boolean                          useForExport;   // set to true if it is ready to be used for export
+    public Integer                          numCols;        // max number of columns we want to allow in csv
+    public Class                            requiredPropertyReader;
+    public FV_PropertyValueWithColumnName   compound[];
 
-    ExportColumnRecord( String cID, String prop, Boolean ufe, Integer nc, Class rpr, Map<String, String> c )
+    ExportColumnRecord( String cID, String prop, Boolean ufe, Integer nc, Class rpr, FV_PropertyValueWithColumnName[] c )
     {
         colID = cID;
         property = prop;
