f1522d22b5059b5d2ed425ea7f605af316b82019
Chad Oakenfold <web@oakenfold.ca>
2019-02-12 15:45:01 -0800

    Adding SEARCH_BY_CUSTOM constant

--- frontend/app/assets/javascripts/views/components/SearchDialect/constants.js
+++ frontend/app/assets/javascripts/views/components/SearchDialect/constants.js
@@ -4,3 +4,4 @@ export const SEARCH_SORT_DEFAULT = 'Any'
 export const SEARCH_BY_DEFAULT = 0
 export const SEARCH_BY_ALPHABET = 1
 export const SEARCH_BY_CATEGORY = 2
+export const SEARCH_BY_CUSTOM = 3
