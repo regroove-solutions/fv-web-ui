ba54e44bc567e22c91c7c52dd31ce8995e5e5a9a
ksubryan <vtr_monk@mac.com>
2019-02-22 12:57:57 -0800

    Stub for listing dialects.

---
+++ nuxeo-addons/FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/operations/ListDialects.java
@@ -0,0 +1,42 @@
+package ca.firstvoices.nuxeo.operations;
+
+
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+
+@Operation(id = ListDialects.ID, category = Constants.CAT_FETCH, label = "List dialect in FV tree.",
+        description = "Returns a list of dialects which can be joined by new users")
+
+public class ListDialects
+{
+    public static final String ID = "Document.ListDialects";
+
+    public static final String ALL_DIALECTS          = "*";
+    public static final String DIALECTS_TO_JOIN      = "to-join";
+    public static final String ENABLED_DIALECTS      = "enabled";
+    public static final String PUBLISHED_DIALECTS    = "published";
+    public static final String DELETED_DIALECTS      = "deleted";
+
+    @Context
+    protected AutomationService automationService;
+
+    @Param(name = "dialect_state", required = true, values = { ALL_DIALECTS, DIALECTS_TO_JOIN, ENABLED_DIALECTS, PUBLISHED_DIALECTS, DELETED_DIALECTS } )
+    protected String dialect_state = DIALECTS_TO_JOIN;
+
+    @OperationMethod
+    public DocumentModelList run() throws OperationException
+    {
+        DocumentModelList dList = null;
+
+
+        return null;
+    }
+}
+
+// "SELECT ecm:uuid, dc:title FROM FVDialect WHERE ecm:currentLifeCycleState <> 'deleted' AND ecm:isLatestVersion = 1 ORDER BY dc:title ASC"
