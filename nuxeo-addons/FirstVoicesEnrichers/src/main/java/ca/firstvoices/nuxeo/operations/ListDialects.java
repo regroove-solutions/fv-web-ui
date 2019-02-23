f7ca9b71ab5ae38ecfbb8c0e2f4f277ef0aefb43
ksubryan <vtr_monk@mac.com>
2019-02-23 10:05:42 -0800

    Changing query to explicit listing of enabled and published dialects.

--- nuxeo-addons/FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/operations/ListDialects.java
+++ nuxeo-addons/FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/operations/ListDialects.java
@@ -14,7 +14,7 @@ import org.nuxeo.runtime.api.Framework;
 
 import javax.security.auth.login.LoginContext;
 
-@Operation(id = ListDialects.ID, category = Constants.CAT_FETCH, label = "List dialects in FV tree.",
+@Operation(id = ListDialects.ID, category = Constants.CAT_DOCUMENT, label = "List dialects in FV tree.",
         description = "Returns a list of dialects which can be joined by new users")
 public class ListDialects
 {
@@ -23,13 +23,14 @@ public class ListDialects
     public static final String ALL_DIALECTS          = "*";
     public static final String DIALECTS_TO_JOIN      = "to-join";
     public static final String ENABLED_DIALECTS      = "enabled";
+    public static final String DISABLED_DIALECTS     = "disabled";
     public static final String PUBLISHED_DIALECTS    = "published";
     public static final String DELETED_DIALECTS      = "deleted";
 
     @Context
     protected AutomationService automationService;
 
-    @Param(name = "dialectState", required = true, values = { DIALECTS_TO_JOIN, ALL_DIALECTS, ENABLED_DIALECTS, PUBLISHED_DIALECTS, DELETED_DIALECTS } )
+    @Param(name = "dialectState", required = true, values = { DIALECTS_TO_JOIN, ALL_DIALECTS, ENABLED_DIALECTS, DISABLED_DIALECTS, PUBLISHED_DIALECTS, DELETED_DIALECTS } )
     protected String dialectState = DIALECTS_TO_JOIN;
 
     @OperationMethod
@@ -41,11 +42,10 @@ public class ListDialects
         DocumentModelList dList = null;
         lctx = Framework.login();
         session = CoreInstance.openCoreSession("default");
+
         String queryFront = "SELECT ecm:uuid, dc:title FROM FVDialect WHERE ";
         String queryEnd = "ecm:isLatestVersion = 1 ORDER BY dc:title ASC";
         String query = queryFront + queryEnd;
-
-
 
         switch( dialectState )
         {
@@ -53,24 +53,26 @@ public class ListDialects
                 query = queryFront + queryEnd;
                 break;
             case DIALECTS_TO_JOIN:
-                query = queryFront + "ecm:currentLifeCycleState <> 'deleted' AND " + queryEnd;
+                query = queryFront + "(ecm:currentLifeCycleState = 'Enabled' OR  ecm:currentLifeCycleState = 'Published') AND " + queryEnd;
                 break;
             case ENABLED_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'enabled' AND " + queryEnd;
+                query = queryFront + "ecm:currentLifeCycleState = 'Enabled' AND " + queryEnd;
+                break;
+            case DISABLED_DIALECTS:
+                query = queryFront + "ecm:currentLifeCycleState = 'Disabled' AND " + queryEnd;
                 break;
             case PUBLISHED_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'published' AND " + queryEnd;
+                query = queryFront + "ecm:currentLifeCycleState = 'Published' AND " + queryEnd;
                 break;
             case DELETED_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'deleted' AND " + queryEnd;
+                query = queryFront + "ecm:currentLifeCycleState = 'Deleted' AND " + queryEnd;
                 break;
         }
 
         dList = session.query( query );
 
         lctx.logout();
-        session.close();
-
+        //session.close();
 
         return dList;
     }
