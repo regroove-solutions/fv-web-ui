2d8bd3645f568828a368ba3e19d313617f7599a2
Mariana Cedica <mcedica@nuxeo.com>
2019-03-05 16:10:03 -0800

    Force blob on return

--- FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/operations/ListDialects.java
+++ FirstVoicesEnrichers/src/main/java/ca/firstvoices/nuxeo/operations/ListDialects.java
@@ -1,34 +1,45 @@
 package ca.firstvoices.nuxeo.operations;
 
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
-import org.nuxeo.ecm.core.api.*;
-import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
-
-import java.io.Serializable;
-import java.util.Iterator;
-import java.util.Map;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.IterableQueryResult;
+import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
-@Operation(id = ListDialects.ID, category = Constants.CAT_DOCUMENT, label = "List dialects in FV tree",
-        description = "Returns a list of dialects based on selection criteria")
-public class ListDialects
-{
+@Operation(id = ListDialects.ID, category = Constants.CAT_DOCUMENT, label = "List dialects in FV tree", description = "Returns a list of dialects based on selection criteria")
+public class ListDialects {
     public static final String ID = "Document.ListDialects";
 
-    public static final String ALL_DIALECTS          = "*";
-    public static final String DIALECTS_TO_JOIN      = "to-join";
-    public static final String NEW_DIALECTS          = "new";
-    public static final String ENABLED_DIALECTS      = "enabled";
-    public static final String DISABLED_DIALECTS     = "disabled";
-    public static final String PUBLISHED_DIALECTS    = "published";
+    private static Log log = LogFactory.getLog(ListDialects.class);
+
+    public static final String ALL_DIALECTS = "*";
+
+    public static final String DIALECTS_TO_JOIN = "to-join";
+
+    public static final String NEW_DIALECTS = "new";
+
+    public static final String ENABLED_DIALECTS = "enabled";
+
+    public static final String DISABLED_DIALECTS = "disabled";
+
+    public static final String PUBLISHED_DIALECTS = "published";
 
     @Context
     protected AutomationService automationService;
@@ -36,55 +47,57 @@ public class ListDialects
     @Context
     protected CoreSession session;
 
-    @Param(name = "dialectState", required = false, values = { DIALECTS_TO_JOIN, ALL_DIALECTS, NEW_DIALECTS, ENABLED_DIALECTS, DISABLED_DIALECTS, PUBLISHED_DIALECTS } )
+    @Param(name = "dialectState", required = false, values = { DIALECTS_TO_JOIN, ALL_DIALECTS, NEW_DIALECTS,
+            ENABLED_DIALECTS, DISABLED_DIALECTS, PUBLISHED_DIALECTS })
     protected String dialectState = DIALECTS_TO_JOIN;
 
     @OperationMethod
-    public Blob run() throws Exception
-    {
+    public Blob run() throws Exception {
         DocumentModelList dList = null;
 
-        String queryFront   = "SELECT ecm:uuid, dc:title FROM FVDialect WHERE ";
+        String queryFront = "SELECT ecm:uuid, dc:title FROM FVDialect WHERE ";
         String queryVersion = "ecm:isVersion=0 ";
-        String queryProxy   = "ecm:isProxy=0 AND ";
-        String querySort    = "ORDER BY dc:title ASC";
+        String queryProxy = "ecm:isProxy=0 AND ";
+        String querySort = "ORDER BY dc:title ASC";
         String query = queryFront + queryVersion + querySort;
 
-        switch( dialectState.toLowerCase() )
-        {
-            case ALL_DIALECTS:
-                query = "SELECT ecm:uuid, dc:title, ecm:currentLifeCycleState FROM FVDialect " + querySort;
-                break;
-            case NEW_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'New' " + querySort;
-                break;
-            case DIALECTS_TO_JOIN:
-                query = queryFront + "(ecm:currentLifeCycleState = 'Enabled' OR  (ecm:currentLifeCycleState = 'Published' AND ecm:isProxy=0)) AND " + queryVersion + querySort;
-                break;
-            case ENABLED_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'Enabled' AND " + queryVersion + querySort;
-                break;
-            case DISABLED_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'Disabled' AND " + queryVersion + querySort;
-                break;
-            case PUBLISHED_DIALECTS:
-                query = queryFront + "ecm:currentLifeCycleState = 'Published' AND " + queryProxy + queryVersion + querySort;
-                break;
-         }
-
+        switch (dialectState.toLowerCase()) {
+        case ALL_DIALECTS:
+            query = "SELECT ecm:uuid, dc:title, ecm:currentLifeCycleState FROM FVDialect " + querySort;
+            break;
+        case NEW_DIALECTS:
+            query = queryFront + "ecm:currentLifeCycleState = 'New' " + querySort;
+            break;
+        case DIALECTS_TO_JOIN:
+            query = queryFront
+                    + "(ecm:currentLifeCycleState = 'Enabled' OR  (ecm:currentLifeCycleState = 'Published' AND ecm:isProxy=0)) AND "
+                    + queryVersion + querySort;
+            break;
+        case ENABLED_DIALECTS:
+            query = queryFront + "ecm:currentLifeCycleState = 'Enabled' AND " + queryVersion + querySort;
+            break;
+        case DISABLED_DIALECTS:
+            query = queryFront + "ecm:currentLifeCycleState = 'Disabled' AND " + queryVersion + querySort;
+            break;
+        case PUBLISHED_DIALECTS:
+            query = queryFront + "ecm:currentLifeCycleState = 'Published' AND " + queryProxy + queryVersion + querySort;
+            break;
+        }
 
         UnrestrictedDialectQuery dialectListQueryRunner = new UnrestrictedDialectQuery(session, query);
         dialectListQueryRunner.runUnrestricted();
 
-
-        return new StringBlob(dialectListQueryRunner.resultSetArray.toString(), "application/json");
+        return Blobs.createJSONBlobFromValue(dialectListQueryRunner.resultSetArray);
     }
 
     protected static class UnrestrictedDialectQuery extends UnrestrictedSessionRunner {
 
         private String query;
+
         private ArrayNode resultSetArray;
+
         protected ObjectMapper mapper;
+
         private IterableQueryResult result;
 
         protected UnrestrictedDialectQuery(CoreSession session, String query) {
@@ -100,16 +113,16 @@ public class ListDialects
 
                 mapper = new ObjectMapper();
                 resultSetArray = mapper.createArrayNode();
-                
+
                 while (it.hasNext()) {
                     Map<String, Serializable> item = it.next();
-                    //JSONObject object = new JSONObject();
-                    //object.accumulateAll(item);
+                    // JSONObject object = new JSONObject();
+                    // object.accumulateAll(item);
                     resultSetArray.add(mapper.writeValueAsString(item));
                 }
             } catch (JsonProcessingException e) {
-                e.printStackTrace();
-			} finally {
+                log.error(e);
+            } finally {
                 result.close();
             }
 
