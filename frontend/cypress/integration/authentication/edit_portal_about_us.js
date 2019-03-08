2d40039881116ecc7c1ac17b96ab705a08dfb60a
Chad Oakenfold <web@oakenfold.ca>
2019-03-08 14:49:53 -0800

    Cypress test now using env't vars

--- frontend/cypress/integration/authentication/edit_portal_about_us.js
+++ frontend/cypress/integration/authentication/edit_portal_about_us.js
@@ -7,13 +7,19 @@ describe('Authentication', () => {
       return false
     })
 
+    // Note: need to set environment variables in your bash_profile, eg:
+    // export CYPRESS_SENCOTEN_USERNAME='THE_USERNAME_FOR_THE_SENCOTEN_TEST_ACCOUNT'
+    // export CYPRESS_SENCOTEN_PASSWORD='THE_PASSWORD_FOR_THE_SENCOTEN_TEST_ACCOUNT'
+    expect(Cypress.env('SENCOTEN_USERNAME')).not.to.be.undefined
+    expect(Cypress.env('SENCOTEN_PASSWORD')).not.to.be.undefined
+
     const options = {
       method: 'POST',
       url: 'https://firstvoices-dev.apps.prod.nuxeo.io/nuxeo/startup',
       form: true, // we are submitting a regular form body
       body: {
-        user_name: 'SENCOTEN_ADMIN',
-        user_password: 'SENCOTEN_ADMIN',
+        user_name: Cypress.env('SENCOTEN_USERNAME'),
+        user_password: Cypress.env('SENCOTEN_PASSWORD'),
         language: 'en',
         requestedUrl: 'app',
         forceAnonymousLogin: true,
