9a61dd99b21d1b112e1d5c2b960c020775b88399
Chad Oakenfold <web@oakenfold.ca>
2019-03-11 12:20:47 -0700

    Pre-commit hook: Test 9

--- frontend/app/assets/javascripts/views/components/JestTestSetup/index.js
+++ frontend/app/assets/javascripts/views/components/JestTestSetup/index.js
@@ -17,21 +17,16 @@ import React from 'react'
 // }
 function JestTestSetup({ onSubmit }) {
   return (
-    <form
-      id="JestTestSetup"
-      className="JestTestSetup"
-      onSubmit={(e) => {
-        e.preventDefault()
-        const { username, password } = e.target.elements
-        onSubmit({
-          username: username.value,
-          password: password.value,
-        })
-      }}
+    <form      id="JestTestSetup"      className="JestTestSetup"      onSubmit={(e) => {
+      e.preventDefault()
+      const { username, password } = e.target.elements
+      onSubmit({
+        username: username.value,
+        password: password.value,
+      })
+    }}
     >
-      <h1>This is the form</h1>
-      <input name="username" type="text" />
-      <input name="password" type="text" />
+      <h1>This is the form</h1>      <input name="username" type="text" />      <input name="password" type="text" />
     </form>
   )
 }
