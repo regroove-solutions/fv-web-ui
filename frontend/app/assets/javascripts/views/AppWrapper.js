2c3d9cbc75e351caf68d105c581fc5b983cbafff
Chad Oakenfold <web@oakenfold.ca>
2019-03-12 14:15:37 -0700

    DEBUG HOOKS > AppWrapper: lint, prettier

--- frontend/app/assets/javascripts/views/AppWrapper.js
+++ frontend/app/assets/javascripts/views/AppWrapper.js
@@ -18,8 +18,6 @@ import React, { Component, PropTypes } from 'react'
 import provide from 'react-redux-provide'
 import selectn from 'selectn'
 
-// import classNames from 'classnames'
-
 import ProviderHelpers from 'common/ProviderHelpers'
 // import StringHelpers from 'common/StringHelpers'
 import UIHelpers from 'common/UIHelpers'
