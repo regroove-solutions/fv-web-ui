240a9659c7d087c6da9695235ccf38aae4d9410c
Mariana Cedica <mcedica@nuxeo.com>
2019-03-15 16:47:52 -0700

    Avoid some exceptions

--- FirstVoicesSecurity/src/main/java/ca/firstvoices/utils/FVLoginUtils.java
+++ FirstVoicesSecurity/src/main/java/ca/firstvoices/utils/FVLoginUtils.java
@@ -1,12 +1,15 @@
 package ca.firstvoices.utils;
 
-import org.nuxeo.ecm.core.api.*;
-import org.nuxeo.ecm.core.query.sql.NXQL;
-import org.nuxeo.ecm.core.schema.FacetNames;
-
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.LifeCycleConstants;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.core.api.impl.DocumentModelListImpl;
+import org.nuxeo.ecm.core.query.sql.NXQL;
+import org.nuxeo.ecm.core.schema.FacetNames;
 
 public class FVLoginUtils {
 
@@ -14,7 +17,7 @@ public class FVLoginUtils {
         DocumentModelList dialects = null;
         List<String> groups = currentUser.getGroups();
 
-        if (groups != null) {
+        if (groups != null && groups.size() >= 1) {
             Iterator it = groups.iterator();
             String inClause = "(\"" + groups.get(0) + "\"";
             it.next();
@@ -26,15 +29,13 @@ public class FVLoginUtils {
             String query = "SELECT * FROM FVDialect WHERE " + NXQL.ECM_MIXINTYPE + " <> '"
                     + FacetNames.HIDDEN_IN_NAVIGATION + "' AND " + NXQL.ECM_LIFECYCLESTATE + " <> '"
                     + LifeCycleConstants.DELETED_STATE + "'" + " AND ecm:isCheckedInVersion = 0 "
-                    + " AND ecm:acl/*/principal IN " + inClause + " "
-                    + " AND ecm:isProxy = 0 ";
+                    + " AND ecm:acl/*/principal IN " + inClause + " " + " AND ecm:isProxy = 0 ";
 
             dialects = session.query(query);
         }
 
-        if( dialects == null )
-        {
-            dialects = (DocumentModelList)new ArrayList<DocumentModel>();
+        if (dialects == null) {
+            dialects = new DocumentModelListImpl();
         }
 
         return dialects;
