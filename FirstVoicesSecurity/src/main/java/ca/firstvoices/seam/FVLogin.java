f6b4533ac0e4526f88b729f84e7d03e86b941a03
Mariana Cedica <mcedica@nuxeo.com>
2019-03-20 12:37:31 -0700

    Fix redirection after login

--- FirstVoicesSecurity/src/main/java/ca/firstvoices/seam/FVLogin.java
+++ FirstVoicesSecurity/src/main/java/ca/firstvoices/seam/FVLogin.java
@@ -1,10 +1,17 @@
 package ca.firstvoices.seam;
 
-import ca.firstvoices.marshallers.CustomPreferencesJSONReader;
-import ca.firstvoices.models.CustomPreferencesObject;
-import ca.firstvoices.utils.FVLoginUtils;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Begin;
 import org.jboss.seam.annotations.Install;
@@ -13,73 +20,60 @@ import org.jboss.seam.annotations.Scope;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.platform.ui.web.rest.RestHelper;
 import org.nuxeo.ecm.webapp.helpers.StartupHelper;
 
-import javax.faces.context.FacesContext;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ca.firstvoices.utils.FVLoginUtils;
 
 @Name("startupHelper")
 @Scope(ScopeType.SESSION)
-@Install(precedence=Install.DEPLOYMENT)
+@Install(precedence = Install.DEPLOYMENT)
 public class FVLogin extends StartupHelper {
     private static final long serialVersionUID = 1L;
+
+    private static final Log log = LogFactory.getLog(FVLogin.class);
 
     @Override
     @Begin(id = "#{conversationIdGenerator.nextMainConversationId}", join = true)
     public String initDomainAndFindStartupPage(String domainTitle, String viewId) {
 
-
-
-        String NUXEO_URL = restHelper.getBaseURL();
-
-        String WEB_UI_URL = NUXEO_URL
-                .replace("/nuxeo", "")
-                .replace("8080", "3001");
-
-        //String SERVLET_PATH = RestHelper.getHttpServletRequest().getServletPath();
-        // SERVLET_PATH.equals("/nxstartup.faces")
-
-        String backToPath = RestHelper.getHttpServletRequest().getParameter("backTo");
-        String result = initServerAndFindStartupPage();
-
-        NuxeoPrincipal currentUser = (NuxeoPrincipal) documentManager.getPrincipal();
-
-        String redirectTo = null;
-
-        try {
-            if (currentUser.isAnonymous()) {
-                redirectTo = NUXEO_URL + "login.jsp";
-            }
-            else if (!currentUser.isAdministrator()) {
-                // A valid "back to" path has been specified
-                if (validatePath(backToPath)) {
-                    redirectTo = WEB_UI_URL + backToPath;
-                }
-                // Otherwise, send to default dialect
-                else {
-                    redirectTo = WEB_UI_URL + getDefaultDialect(currentUser);
-                }
-
-            }
-
-            if (redirectTo != null) {
-                FacesContext.getCurrentInstance().getExternalContext().redirect(redirectTo);
-                return null;
-            }
-        } catch (IOException e) {
-            return dashboardNavigationHelper.navigateToDashboard();
+        if (documentManager == null) {
+            super.initServerAndFindStartupPage();
         }
 
-        return dashboardNavigationHelper.navigateToDashboard();
-    }
+        String NUXEO_URL = restHelper.getBaseURL();
+        String redirectTo = NUXEO_URL + "app/";
+        String backToPath = RestHelper.getHttpServletRequest().getParameter("backTo");
 
+        NuxeoPrincipal currentUser = documentManager.getPrincipal();
+        if (currentUser.isAdministrator()) {
+            return dashboardNavigationHelper.navigateToDashboard();
+        }
+        if (!currentUser.isAdministrator() && !currentUser.isAnonymous()) {
+            if (validatePath(backToPath)) {
+                redirectTo = NUXEO_URL + backToPath;
+            }
+            // Otherwise, send to default dialect
+            else {
+                String dialect = getDefaultDialect(currentUser);
+                redirectTo = NUXEO_URL + (dialect == null ? "app/" : dialect);
+            }
+        }
+
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect(redirectTo);
+            return null;
+        } catch (IOException e) {
+            log.error(e);
+            throw new NuxeoException(e);
+        }
+
+    }
 
     private String getDefaultDialect(NuxeoPrincipal currentUser) {
         String primary_dialect_path = null;
@@ -87,52 +81,47 @@ public class FVLogin extends StartupHelper {
         String userPreferences = (String) currentUser.getModel().getPropertyValue("user:preferences");
 
         if (currentUser.isAnonymous()) {
-            return "";
+            return null;
         }
 
         // Lookup dialect in default preferences
         if (userPreferences != null) {
+            DocumentModel dialect = null;
+
+            Map<String, Map<String, Object>> userPreferencesSettings;
             try {
-                DocumentModel dialect = null;
-
-                CustomPreferencesJSONReader prefReader = new CustomPreferencesJSONReader();
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode actualObj = mapper.readTree(userPreferences);
-
-                CustomPreferencesObject userPreferencesModel = prefReader.read(actualObj);
-
-                Object primary_dialect_obj = userPreferencesModel.getGeneralPreferences().get("primary_dialect");
-
-                if (primary_dialect_obj != null){
-                    String primary_dialect = primary_dialect_obj.toString();
-                    dialect = documentManager.getDocument(new IdRef(primary_dialect));
-                }
-
-                if (dialect != null) {
-                    Object primary_dialect_short_url_obj = dialect.getPropertyValue("fvdialect:short_url");
-
-                    if (primary_dialect_short_url_obj != null) {
-                        primary_dialect_short_url = primary_dialect_short_url_obj.toString();
-                    }
-
-                    primary_dialect_path = dialect.getPathAsString();
-                }
+                userPreferencesSettings = new ObjectMapper().readValue(userPreferences,
+                        new TypeReference<Map<String, Map<String, Object>>>() {
+                        });
             } catch (IOException e) {
-                e.printStackTrace();
+                log.error(e);
+                throw new NuxeoException(e);
+            }
+
+            String primary_dialect_obj = (String) userPreferencesSettings.get("General").get("primary_dialect");
+            if (primary_dialect_obj != null && documentManager.exists(new IdRef(primary_dialect_obj))) {
+                // MC-Nuxeo : I see that new inviated users don't have acess to this document even if the dialect its
+                // set in theire preferences
+                dialect = documentManager.getDocument(new IdRef(primary_dialect_obj));
+            }
+
+            if (dialect != null) {
+                primary_dialect_short_url = (String) dialect.getPropertyValue("fvdialect:short_url");
+                primary_dialect_path = dialect.getPathAsString();
             }
         }
 
         // Find first dialect user is member of...
         else {
             DocumentModelList dialects = FVLoginUtils.getDialectsForUser(currentUser, documentManager);
-
+            // MC-Nuxeo: fvdialect:short_url is always null; where is this set?
             if (dialects.size() > 0) {
-                primary_dialect_short_url = dialects.get(0).getPropertyValue("fvdialect:short_url").toString();
+                primary_dialect_short_url = (String) dialects.get(0).getPropertyValue("fvdialect:short_url");
                 primary_dialect_path = dialects.get(0).getPathAsString();
             }
         }
 
-        String finalPath = "";
+        String finalPath = null;
 
         if (primary_dialect_short_url != null && !primary_dialect_short_url.isEmpty()) {
             // Users who are global 'members' should just go to the Sections URL
@@ -143,10 +132,9 @@ public class FVLogin extends StartupHelper {
             else {
                 finalPath = "Workspaces/" + primary_dialect_short_url;
             }
-        }
-        else if (primary_dialect_path != null) {
+        } else if (primary_dialect_path != null) {
             try {
-                finalPath = "explore" + URLDecoder.decode(primary_dialect_path, StandardCharsets.UTF_8.name());
+                finalPath = "app/explore" + URLDecoder.decode(primary_dialect_path, StandardCharsets.UTF_8.name());
             } catch (UnsupportedEncodingException e) {
                 // TODO: capture in audit log?
             }
@@ -156,17 +144,15 @@ public class FVLogin extends StartupHelper {
     }
 
     private boolean validatePath(String path) {
-        String URL_REGEX =
-                "^((%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)" +
-                "([).!';/?:,][[:blank:]])?$";
+        String URL_REGEX = "^((%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)" + "([).!';/?:,][[:blank:]])?$";
 
         Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
-            if (path == null || path.isEmpty()) {
-                return false;
-            }
+        if (path == null || path.isEmpty()) {
+            return false;
+        }
 
-            Matcher matcher = URL_PATTERN.matcher(path);
-            return matcher.matches();
+        Matcher matcher = URL_PATTERN.matcher(path);
+        return matcher.matches();
     }
 }
