9e23efce251fa7b9151cb63acc374641c76c0b84
Chad Oakenfold <web@oakenfold.ca>
2019-03-21 16:54:55 -0700

    DirectoryList: added a data-testid hook

--- frontend/app/assets/javascripts/views/components/Editor/DirectoryList.js
+++ frontend/app/assets/javascripts/views/components/Editor/DirectoryList.js
@@ -13,91 +13,89 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import React, {Component, PropTypes} from 'react';
-import provide from 'react-redux-provide';
-import selectn from 'selectn';
+import React, { Component, PropTypes } from 'react'
+import provide from 'react-redux-provide'
+import selectn from 'selectn'
 
-import SelectField from 'material-ui/lib/SelectField';
-import MenuItem from 'material-ui/lib/menus/menu-item';
+import SelectField from 'material-ui/lib/SelectField'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import StringHelpers, { CLEAN_ID } from 'common/StringHelpers'
+import IntlService from 'views/services/intl'
+const intl = IntlService.instance
 
-import IntlService from 'views/services/intl';
-const intl = IntlService.instance;
+export class DirectoryList extends Component {
+  static propTypes = {
+    fetchDirectory: PropTypes.func.isRequired,
+    computeDirectory: PropTypes.object.isRequired,
+    directory: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+    fancy: PropTypes.bool,
+    value: PropTypes.string,
+    dataTestId: PropTypes.string,
+  }
 
-@provide
-export default class DirectoryList extends Component {
+  static defaultProps = {
+    fancy: true,
+  }
 
-    static propTypes = {
-        fetchDirectory: PropTypes.func.isRequired,
-        computeDirectory: PropTypes.object.isRequired,
-        directory: PropTypes.string.isRequired,
-        onChange: PropTypes.func.isRequired,
-        label: PropTypes.string.isRequired,
-        fancy: PropTypes.bool,
-        value: PropTypes.string
-    };
+  constructor(props) {
+    super(props)
 
-    static defaultProps = {
-        fancy: true
-    };
+    this._handleChange = this._handleChange.bind(this)
+    this._handleStandardSelectChange = this._handleStandardSelectChange.bind(this)
+  }
 
-    constructor(props) {
-        super(props);
+  _handleChange(event, index, value) {
+    this.props.onChange(value)
+  }
 
-        this._handleChange = this._handleChange.bind(this);
-        this._handleStandardSelectChange = this._handleStandardSelectChange.bind(this);
+  _handleStandardSelectChange(event) {
+    this.props.onChange(event.target.value)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // Ensure value is in sync -- relevant for setting default value dynamically
+    if (nextProps.value !== undefined) {
+      this.props.onChange(nextProps.value)
     }
+  }
 
-    _handleChange(event, index, value) {
-        this.props.onChange(value);
-    }
+  componentDidMount() {
+    this.props.fetchDirectory(this.props.directory)
+  }
 
-    _handleStandardSelectChange(event) {
-        this.props.onChange(event.target.value);
-    }
+  render() {
+    const { computeDirectory } = this.props
 
-    componentWillReceiveProps(nextProps) {
-        // Ensure value is in sync -- relevant for setting default value dynamically
-        if (nextProps.value != undefined) {
-            this.props.onChange(nextProps.value);
-        }
-    }
-
-    componentDidMount() {
-        this.props.fetchDirectory(this.props.directory);
-    }
-
-    render() {
-
-        let previewStyles = {
-            padding: '10px'
-        }
-
-        const {computeDirectory} = this.props;
-
-        let entries = selectn('directories.' + this.props.directory, computeDirectory) || [];
-
-        return (
-            <div>
-                {
-                    (this.props.fancy) ?
-
-                        <SelectField maxHeight={300} autoWidth={true} value={this.props.value}
-                                     onChange={this._handleChange}
-                                     floatingLabelText={intl.trans('select_x', 'Select ' + this.props.label, 'first', [this.props.label]) + ':'}>
-                            {entries.map((entry) =>
-                                <MenuItem key={entry.value} value={entry.value} primaryText={entry.text}/>
-                            )}
-                        </SelectField>
-
-                        :
-
-                        <select value={this.props.value} onChange={this._handleStandardSelectChange}>
-                            {entries.map((entry) =>
-                                <option key={entry.value} value={entry.value}>{entry.text}</option>
-                            )}
-                        </select>
-                }
-            </div>
-        );
-    }
+    const entries = selectn('directories.' + this.props.directory, computeDirectory) || []
+    const dataTestId = StringHelpers.clean(this.props.dataTestId, CLEAN_ID)
+    return (
+      <div>
+        {this.props.fancy ? (
+          <SelectField
+            data-testid={dataTestId}
+            maxHeight={300}
+            autoWidth
+            value={this.props.value}
+            onChange={this._handleChange}
+            floatingLabelText={intl.trans('select_x', 'Select ' + this.props.label, 'first', [this.props.label]) + ':'}
+          >
+            {entries.map((entry) => (
+              <MenuItem key={entry.value} value={entry.value} primaryText={entry.text} />
+            ))}
+          </SelectField>
+        ) : (
+          <select data-testid={dataTestId} onChange={this._handleStandardSelectChange}>
+            {entries.map((entry) => (
+              <option key={entry.value} value={entry.value}>
+                {entry.text}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+    )
+  }
 }
+export default provide(DirectoryList)
