1fc4448e04ff480957cbfc2e2d95e377544d348a
Chad Oakenfold <web@oakenfold.ca>
2019-03-20 22:32:43 -0700

    virtualKeyboard: data-testid

--- frontend/app/assets/javascripts/views/components/Editor/fields/virtualKeyboard.js
+++ frontend/app/assets/javascripts/views/components/Editor/fields/virtualKeyboard.js
@@ -1,25 +1,39 @@
-import React, {Component, PropTypes} from 'react';
-import t from 'tcomb-form';
+import React from 'react'
+import t from 'tcomb-form'
+import StringHelpers, { CLEAN_ID } from 'common/StringHelpers'
 
 function renderInput(locals) {
-
-    const onChange = function (event) {
-        locals.onChange(event.target.value);
-    };
-
-    return <div>
-        <input type="text" label={locals.label} name={locals.attrs.name} value={locals.value} onChange={onChange}
-               style={{display: 'none'}}/>
-        <input type="text" id={'virtual-keyboard-helper-' + locals.attrs.name} value={locals.value}
-               className="form-control" label={locals.label} onChange={onChange} onBlur={onChange}/>
-    </div>;
+  const onChange = function virtualKeyboardOnChange(event) {
+    locals.onChange(event.target.value)
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        label={locals.label}
+        name={locals.attrs.name}
+        value={locals.value}
+        onChange={onChange}
+        style={{ display: 'none' }}
+      />
+      <input
+        type="text"
+        data-testid={StringHelpers.clean(locals.attrs.name, CLEAN_ID)}
+        id={'virtual-keyboard-helper-' + StringHelpers.clean(locals.attrs.name, CLEAN_ID)}
+        value={locals.value}
+        className="form-control"
+        label={locals.label}
+        onChange={onChange}
+        onBlur={onChange}
+      />
+    </div>
+  )
 }
 
-const textboxTemplate = t.form.Form.templates.textbox.clone({renderInput})
+const textboxTemplate = t.form.Form.templates.textbox.clone({ renderInput })
 
 export default class VirtualKeyboardFactory extends t.form.Textbox {
-
-    getTemplate() {
-        return textboxTemplate
-    }
+  getTemplate() {
+    return textboxTemplate
+  }
 }
