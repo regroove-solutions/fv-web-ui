04eae59e9da2615df371710c6ffce25fd140ee98
Chad Oakenfold <web@oakenfold.ca>
2019-03-21 16:58:07 -0700

    E2E: word editing touches most fields/modals

--- frontend/cypress/integration/word_crud.js
+++ frontend/cypress/integration/word_crud.js
@@ -1,11 +1,18 @@
 describe('Word', () => {
+  // NOTE: turn off all uncaught exception handling
+  cy.on('uncaught:exception', () => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
+
   const host = 'http://0.0.0.0:3001'
   //   const page = `${host}/nuxeo/app/explore/FV/Workspaces/Data/Athabascan/Dene/Dene/learn/words/f1464526-9022-4103-9174-9a7fffc71419`
   const login = 'https://firstvoices-dev.apps.prod.nuxeo.io/nuxeo/startup'
   const create =
     'https://firstvoices-dev.apps.prod.nuxeo.io/nuxeo/api/v1/path/FV/Workspaces/Data/Athabascan/Dene/Dene/Dictionary'
   const prefix = '/nuxeo/app'
-  const waitLong = 5000
+  // const waitLong = 5000
   const waitMedium = 2000
   const waitShort = 50
   it('Edit', () => {
@@ -18,7 +25,7 @@ describe('Word', () => {
     expect(Cypress.env('ADMIN_PASSWORD')).not.to.be.undefined
 
     // Login
-    cy.log('Login')
+    cy.log('--- LOGIN ---')
     cy.request({
       method: 'POST',
       url: login,
@@ -35,7 +42,8 @@ describe('Word', () => {
     })
 
     // Create
-    cy.log('Word > create')
+    cy.log('--- CREATE ---')
+
     const nowCreate = Date.now()
     const title = `Cypress: Word > CRUD | Test ran at ${nowCreate}`
     const word = {
@@ -53,30 +61,184 @@ describe('Word', () => {
       url: create,
       body: word,
     }).then((response) => {
-      // Visit
       const page = `${host}${prefix}/explore/FV/Workspaces/Data/Athabascan/Dene/Dene/learn/words/${response.body.uid}`
       cy.visit(page)
-      cy.log('Word > read')
+
+      // Read
+      cy.log('--- READ ---')
+
       cy.wait(waitMedium)
-      cy.getByText(title, { exact: false })
+      cy.getByText(title).should('exist')
 
       // Update
-      cy.log('Word > update')
+      cy.log('--- UPDATE ---')
       cy.getByText('Edit word', { exact: false }).click()
-      const nowEdit = Date.now()
-      cy.get('input[label="Reference"].form-control').type(nowEdit)
+
+      // UPDATE > Word
+      cy.getByTestId('dc-title')
+        .clear()
+        .type('UPDATE > Word')
+
+      // UPDATE > Part of speech
+      cy.getByTestId('fv-word-part_of_speech').select('question_word')
+
+      // UPDATE > Pronounciation
+      cy.getByTestId('fv-word-pronunciation')
+        .clear()
+        .type('UPDATE > Pronounciation')
+
+      // UPDATE > Definition
+      cy.getByText('+ Add definition', { exact: false }).click()
+      cy.getByTestId('fv-definitions0translation').type('UPDATE > Definition')
+
+      // UPDATE > Literal Translation
+      cy.getByText('+ Add literal translation', { exact: false }).click()
+      cy.getByTestId('fv-literal_translation0translation').type('UPDATE > Literal Translation')
+
+      // UPDATE > Audio
+      cy.getByText('+ Add related audio', { exact: false })
+        .parents('button')
+        .click()
+
+      cy.getByText('upload audio', { exact: false }).click()
+
+      cy.getByText('Create new audio in the dene dialect', { exact: false })
+        .parent()
+        .within(() => {
+          cy.getByLabelText('name', { exact: false }).type('UPDATE > AUDIO > NAME')
+          cy.getByLabelText('description', { exact: false }).type('UPDATE > AUDIO > DESCRIPTION')
+          // NOTE: Not certain how to (or if should) test uploading of audio
+          // cy.getByLabelText('FILE', { exact: false }).click()
+          cy.getByLabelText('Shared accross dialects', { exact: false }).check()
+          cy.getByLabelText('Child focused', { exact: false }).check()
+          cy.getByText('cancel', { exact: false }).click()
+        })
+
+      cy.getByText('Related audio', { exact: false })
+        .parents('.row')
+        .within(() => {
+          cy.getByText('clear', { exact: false }).click()
+        })
+
+      // UPDATE > picture
+      cy.getByText('+ Add related pictures', { exact: false })
+        .parents('button')
+        .click()
+
+      cy.getByText('upload picture', { exact: false }).click()
+
+      cy.getByText('Create new picture in the dene dialect', { exact: false })
+        .parent()
+        .within(() => {
+          cy.getByLabelText('name', { exact: false }).type('UPDATE > PICTURE > NAME')
+          cy.getByLabelText('description', { exact: false }).type('UPDATE > PICTURE > DESCRIPTION')
+          cy.getByLabelText('Shared accross dialects', { exact: false }).check()
+          cy.getByLabelText('Child focused', { exact: false }).check()
+          cy.getByText('cancel', { exact: false }).click()
+        })
+
+      cy.getByText('Related pictures', { exact: false })
+        .parents('.row')
+        .within(() => {
+          cy.getByText('clear', { exact: false }).click()
+        })
+
+      // UPDATE > video
+      cy.getByText('+ Add related videos', { exact: false })
+        .parents('button')
+        .click()
+
+      cy.getByText('upload video', { exact: false }).click()
+
+      cy.getByText('Create new video in the dene dialect', { exact: false })
+        .parent()
+        .within(() => {
+          cy.getByLabelText('name', { exact: false }).type('UPDATE > VIDEO > NAME')
+          cy.getByLabelText('description', { exact: false }).type('UPDATE > VIDEO > DESCRIPTION')
+          cy.getByLabelText('Shared accross dialects', { exact: false }).check()
+          cy.getByLabelText('Child focused', { exact: false }).check()
+          cy.getByText('cancel', { exact: false }).click()
+        })
+
+      cy.getByText('Related videos', { exact: false })
+        .parents('.row')
+        .within(() => {
+          cy.getByText('clear', { exact: false }).click()
+        })
+
+      // UPDATE > phrases
+      cy.getByText('+ Add related phrases', { exact: false }).click()
+
+      cy.getByText('create new phrase', { exact: false }).click()
+
+      cy.getByText('Add New Phrase to Dene', { exact: false })
+        .parent()
+        .parent()
+        .parent()
+        .within(() => {
+          cy.getByLabelText('phrase', { exact: false }).type('UPDATE > PHRASE')
+          cy.getByText('cancel', { exact: false }).click()
+        })
+
+      cy.getByText('Related phrases', { exact: false })
+        .parent('fieldset')
+        .within(() => {
+          cy.getByText('X', { exact: false }).click()
+        })
+
+      // UPDATE > Category
+      cy.getByText('+ Add categories', { exact: false }).click()
+      cy.getByText('Browse categories', { exact: false }).click()
+
+      cy.getByText('Select Categories', { exact: false })
+        .parent()
+        .within(() => {
+          cy.getByText('cancel', { exact: false }).click()
+        })
+
+      cy.getByText('Categories', { exact: false })
+        .parent('fieldset')
+        .within(() => {
+          cy.getByText('X', { exact: false }).click()
+        })
+
+      // UPDATE > Cultural Note
+      cy.getByText('Cultural note', { exact: false })
+        .parent('fieldset')
+        .within(() => {
+          cy.getByText('+ Add cultural note', { exact: false }).click()
+          cy.getByText('X', { exact: false }).click()
+        })
+      // UPDATE > Reference
+      cy.get('input[label="Reference"].form-control').type('UPDATE > Reference')
+
+      // UPDATE > Source
+      cy.getByText('Source', { exact: false })
+        .parent('fieldset')
+        .within(() => {
+          cy.getByText('+ Add source', { exact: false }).click()
+          cy.getByText('X', { exact: false }).click()
+        })
+      // UPDATE > children's archive
+      cy.getByLabelText("Available in children's archive", { exact: false }).check()
+      // UPDATE > games
+      cy.getByLabelText('Available in games', { exact: false }).check()
+
+      // UPDATE: save
       cy.getByText('save', { exact: false }).click()
 
-      cy.log(
-        `Using an explicit cy.wait(${waitLong}). Suspect when testing a SPA, the 'load' event is unreliable indicator of done`
-      )
-      cy.wait(waitLong)
-      cy.getByText(title).should('exist')
-      cy.getByText('METADATA', { exact: false }).click()
-      cy.get('aside').contains(nowEdit)
+      cy.wait(waitMedium)
 
-      // Delete
-      cy.log('Word > delete')
+      // UPDATE: verify
+      cy.getByText('UPDATE > Word').should('exist')
+      cy.getByText('UPDATE > Definition').should('exist')
+      cy.getByText('UPDATE > Literal Translation').should('exist')
+      cy.getByText('UPDATE > Pronounciation').should('exist')
+      cy.getByText('Question word').should('exist')
+
+      // DELETE
+      cy.log('--- DELETE ---')
+
       cy.getByText('delete word', { exact: false }).click()
       cy.wait(waitShort)
       cy.getByText('Deleting word', { exact: false })
