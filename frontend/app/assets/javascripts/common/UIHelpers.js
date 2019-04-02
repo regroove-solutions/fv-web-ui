1c3f9b404bedd8f232671fc036617a9fe746a9fd
Chad Oakenfold <web@oakenfold.ca>
2019-04-02 10:37:22 -0700

    Fixing a bad merge

--- frontend/app/assets/javascripts/common/UIHelpers.js
+++ frontend/app/assets/javascripts/common/UIHelpers.js
@@ -13,136 +13,79 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+import React from 'react'
 import ConfGlobal from 'conf/local.json'
 import selectn from 'selectn'
 import NavigationHelpers from 'common/NavigationHelpers'
 
 export default {
-    renderComplexArrayRow(dataItems = [], render) {
-        let rows = [];
+  renderComplexArrayRow(dataItems = [], render) {
+    const rows = []
 
-        dataItems.map(function (entry, i) {
-            rows.push(render(entry, i));
-        });
+    dataItems.map(function dataItemsMap(entry, i) {
+      rows.push(render(entry, i))
+    })
 
-        return <ol style={{
-            'fontSize': '0.9em',
-            'margin': 0,
-            'padding': (rows.length == 1) ? '0' : '0 15px',
-            'listStyle': (rows.length == 1) ? 'none' : 'decimal'
-        }}>
-            {rows}
-        </ol>;
-    },
-    getPreferenceVal(key, preferences) {
-        return selectn('preferences.values.' + key + '.' + selectn(key, preferences), ConfGlobal);
-    },
-    getThumbnail(imgObj, view = 'Thumbnail', returnObj = false) {
+    return (
+      <ol
+        style={{
+          fontSize: '0.9em',
+          margin: 0,
+          padding: rows.length == 1 ? '0' : '0 15px',
+          listStyle: rows.length == 1 ? 'none' : 'decimal',
+        }}
+      >
+        {rows}
+      </ol>
+    )
+  },
+  getPreferenceVal(key, preferences) {
+    return selectn('preferences.values.' + key + '.' + selectn(key, preferences), ConfGlobal)
+  },
+  getThumbnail(imgObj, view = 'Thumbnail', returnObj = false) {
+    let i = 0
 
-        let i = 0;
+    switch (view) {
+      case 'Thumbnail':
+        i = 0
+        break
 
-        switch (view) {
-            case 'Thumbnail':
-                i = 0;
-                break;
+      case 'Small':
+        i = 1
+        break
 
-            case 'Small':
-                i = 1;
-                break;
+      case 'Medium':
+        i = 2
+        break
 
-            case 'Medium':
-                i = 2;
-                break;
-
-            case 'OriginalJpeg':
-                i = 3;
-                break;
-        }
-
-        if (selectn('views[' + i + ']', imgObj)) {
-            return (returnObj) ? selectn('views[' + i + ']', imgObj) : selectn('views[' + i + '].url', imgObj);
-        } else if (selectn('properties.picture:views[' + i + ']', imgObj)) {
-            return (returnObj) ? selectn('properties.picture:views[' + i + ']', imgObj) : selectn('properties.picture:views[' + i + '].content.data', imgObj);
-        } else if (selectn('properties.file:content.data', imgObj)) {
-            return (returnObj) ? selectn('properties.file:content.data', imgObj) : selectn('properties.file:content.data', imgObj);
-        } else if (selectn('path', imgObj)) {
-            return NavigationHelpers.getBaseURL() + selectn('path', imgObj).replace("nxfile", "nxpicsfile").replace("file:", view + ":");
-        } else if (selectn('data', imgObj)) {
-            return selectn('data', imgObj);
-        }
-
-        return 'assets/images/cover.png';
-    },
-    playAudio(state, stateFunc, audioUrl, e) {
-
-        if (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-
-        let audioItem = new Audio(audioUrl);
-
-        if (state.nowPlaying != null) {
-            state.nowPlaying.pause();
-            state.currentTime = 0;
-        }
-
-        stateFunc({
-            nowPlaying: audioItem
-        });
-
-        audioItem.play();
-
-        audioItem.onended = function () {
-            stateFunc({
-                nowPlaying: null
-            });
-        };
-
-        return false;
-    },
-    stopAudio(state, stateFunc, e) {
-
-        if (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-
-        if (state.nowPlaying != null) {
-            state.nowPlaying.pause();
-            state.currentTime = 0;
-
-            stateFunc({
-                nowPlaying: null
-            });
-        }
-
-        return false;
-    },
-    isViewSize(size) {
-        switch (size) {
-            case 'xs':
-                return window.innerWidth <= 420
-                break;
-        }
-
-        return false;
+      case 'OriginalJpeg':
+        i = 3
+        break
+      default: // Note: do nothing
     }
 
     if (selectn('views[' + i + ']', imgObj)) {
-      return (returnObj) ? selectn('views[' + i + ']', imgObj) : selectn('views[' + i + '].url', imgObj)
+      return returnObj ? selectn('views[' + i + ']', imgObj) : selectn('views[' + i + '].url', imgObj)
     } else if (selectn('properties.picture:views[' + i + ']', imgObj)) {
-      return (returnObj) ? selectn('properties.picture:views[' + i + ']', imgObj) : selectn('properties.picture:views[' + i + '].content.data', imgObj)
+      return returnObj
+        ? selectn('properties.picture:views[' + i + ']', imgObj)
+        : selectn('properties.picture:views[' + i + '].content.data', imgObj)
     } else if (selectn('properties.file:content.data', imgObj)) {
-      return (returnObj) ? selectn('properties.file:content.data', imgObj) : selectn('properties.file:content.data', imgObj)
+      return returnObj
+        ? selectn('properties.file:content.data', imgObj)
+        : selectn('properties.file:content.data', imgObj)
     } else if (selectn('path', imgObj)) {
-      return NavigationHelpers.getBaseURL() + selectn('path', imgObj).replace('nxfile', 'nxpicsfile').replace('file:', view + ':')
+      return (
+        NavigationHelpers.getBaseURL() +
+        selectn('path', imgObj)
+          .replace('nxfile', 'nxpicsfile')
+          .replace('file:', view + ':')
+      )
     } else if (selectn('data', imgObj)) {
       return selectn('data', imgObj)
     }
 
-    return '/assets/images/cover.png'
+    return 'assets/images/cover.png'
   },
   playAudio(state, stateFunc, audioUrl, e) {
     if (e) {
@@ -163,7 +106,7 @@ export default {
 
     audioItem.play()
 
-    audioItem.onended = function() {
+    audioItem.onended = function audioItemOnended() {
       stateFunc({
         nowPlaying: null,
       })
@@ -192,7 +135,7 @@ export default {
     switch (size) {
       case 'xs':
         return window.innerWidth <= 420
-        break
+      default: // Note: do nothing
     }
 
     return false
