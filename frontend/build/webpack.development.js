9cbc7f8861d6f55dd9a867231e3215a9aa4985ae
Daniel Yona <daniel@fpcc.ca>
2019-04-16 11:43:40 -0700

    Add analytics code to index.html

--- frontend/build/webpack.development.js
+++ frontend/build/webpack.development.js
@@ -5,6 +5,7 @@ const common = require("./webpack.common");
  * Development Webpack Configuration
  */
 module.exports = env => merge(common(env), {
+    mode: "development",
     output: {
         publicPath: "/"
     },
