d19bb96afce2072162d8f6f92a6644f48f0544ba
Chad Oakenfold <web@oakenfold.ca>
2019-05-02 12:02:49 -0700

    Definitions
    Added support for setting items via props; relocated move/delete btns

--- frontend/app/assets/javascripts/views/pages/explore/dialect/learn/words/CreateV2/FormDefinitions.js
+++ frontend/app/assets/javascripts/views/pages/explore/dialect/learn/words/CreateV2/FormDefinitions.js
@@ -5,24 +5,32 @@ import Select from './Select'
 import FormMoveButtons from './FormMoveButtons'
 import FormRemoveButton from './FormRemoveButton'
 import { removeItem, moveItemDown, moveItemUp } from './FormInteractions'
-const { string } = PropTypes
+const { string, array, func } = PropTypes
 
 export default class FormDefinitions extends React.Component {
   static defaultProps = {
     className: 'FormDefinitions',
     groupName: 'Form__group',
     name: 'FormDefinitions',
+    itemData: [],
+    handleChange: () => {},
   }
 
   static propTypes = {
     name: string.isRequired,
     className: string,
     groupName: string,
+    itemData: array,
+    handleChange: func,
   }
 
   state = {
     items: [], // jsx
     itemData: {},
+  }
+
+  componentDidMount() {
+    this._populateItems()
   }
 
   render() {
@@ -35,16 +43,16 @@ export default class FormDefinitions extends React.Component {
         <legend>Definitions</legend>
         <p className="alert alert-info">Describe what the word actually means</p>
 
+        {items}
+
         <button
           type="button"
           onClick={() => {
-            this.handleClickAddItem()
+            this._handleClickAddItem()
           }}
         >
           Add a Definition
         </button>
-
-        {items}
 
         {this._generateHiddenInput()}
 
@@ -64,75 +72,107 @@ Definition up' and 'Move Definition down' buttons`}
     )
   }
 
-  handleClickAddItem = () => {
+  _handleClickAddItem = () => {
     const items = this.state.items
     const { className } = this.props
     const id = `${className}_${items.length}_${Date.now()}`
 
-    items.push(
-      <fieldset key={id} id={id} className={this.props.groupName}>
-        <legend>Definition</legend>
-
-        <div className="FormItemButtons">
-          <FormMoveButtons
-            id={id}
-            idDescribedByItemMove={'describedByDefinitionMove'}
-            textBtnMoveItemUp={'Move Definition up'}
-            textBtnMoveItemDown={'Move Definition down'}
-            handleClickMoveItemUp={this.handleClickMoveItemUp}
-            handleClickMoveItemDown={this.handleClickMoveItemDown}
-          />
-          <FormRemoveButton
-            id={id}
-            textBtnRemoveItem={'Remove Definition'}
-            handleClickRemoveItem={this.handleClickRemoveItem}
-          />
-        </div>
-
-        <Select
-          ariaDescribedby="describedbyLanguage"
-          className="DefinitionLanguage"
-          id={`definitions.${items.length}.language`}
-          labelText="Language"
-          name="" // Note: intentionally generating invalid name so won't be picked up by `new FormData(this.form)`
-          handleChange={(value) => {
-            this._handleChange({
-              id,
-              value,
-              property: 'language',
-            })
-          }}
-          value="english"
-        >
-          {/* Note: Using optgroup until React 16 when can use Fragments, eg: <React.Fragment> or <> */}
-          <optgroup>
-            <option value="english">English</option>
-            <option value="french">French</option>
-            <option value="mandarin">Mandarin</option>
-          </optgroup>
-        </Select>
-        <Text
-          className="Create__DefinitionTranslation"
-          ariaDescribedby="describedByTranslation"
-          id={`definitions.${items.length}.translation`}
-          labelText="Translation"
-          name="" // Note: intentionally generating invalid name so won't be picked up by `new FormData(this.form)`
-          handleChange={(value) => {
-            this._handleChange({
-              id,
-              value,
-              property: 'translation',
-            })
-          }}
-        />
-      </fieldset>
-    )
+    items.push(this._generateDefinition({ id, index: items.length }))
     const { itemData } = this.state
     itemData[id] = { language: 'english', translation: '' }
-    this.setState({
-      items,
-      itemData,
+    this.setState(
+      {
+        items,
+        itemData,
+      },
+      () => {
+        // focus on the newly created select
+        this[`${id}.select`].focus()
+      }
+    )
+  }
+
+  _populateItems = () => {
+    const { className, itemData: itemDataProps } = this.props
+    const { items, itemData } = this.state
+    const _items = []
+    const _itemData = {}
+    itemDataProps.forEach((item, index) => {
+      const { language, translation } = item
+      const id = `${className}_${items.length + index}_${Date.now()}`
+      _items.push(this._generateDefinition({ id, language, translation, index }))
+      _itemData[id] = { language, translation }
     })
+    this.setState({
+      items: [...items, ..._items],
+      itemData: Object.assign({}, itemData, _itemData),
+    })
+  }
+
+  _generateDefinition = ({ id, index, language, translation }) => {
+    return (
+      <fieldset key={id} id={id} className={this.props.groupName}>
+        <legend>Definition</legend>
+        <div className="Form__sidebar">
+          <div className="Form__main">
+            <Select
+              ariaDescribedby="describedbyLanguage"
+              className="DefinitionLanguage"
+              id={`definitions.${index}.language`}
+              labelText="Language"
+              name="" // Note: intentionally generating invalid name so won't be picked up by `new FormData(this.form)`
+              handleChange={(value) => {
+                this._handleChange({
+                  id,
+                  value,
+                  property: 'language',
+                })
+              }}
+              value={language || 'english'}
+              setRef={(element) => (this[`${id}.select`] = element)}
+            >
+              {/* Note: Using optgroup until React 16 when can use Fragments, eg: <React.Fragment> or <> */}
+              <optgroup>
+                <option value="english">English</option>
+                <option value="french">French</option>
+                <option value="mandarin">Mandarin</option>
+              </optgroup>
+            </Select>
+            <Text
+              className="Create__DefinitionTranslation"
+              ariaDescribedby="describedByTranslation"
+              id={`definitions.${index}.translation`}
+              labelText="Translation"
+              name="" // Note: intentionally generating invalid name so won't be picked up by `new FormData(this.form)`
+              handleChange={(value) => {
+                this._handleChange({
+                  id,
+                  value,
+                  property: 'translation',
+                })
+              }}
+              value={translation || ''}
+            />
+          </div>
+
+          <div className="FormItemButtons Form__aside">
+            <FormRemoveButton
+              id={id}
+              textBtnRemoveItem={'Remove Definition'}
+              handleClickRemoveItem={this.handleClickRemoveItem}
+            />
+            <FormMoveButtons
+              id={id}
+              idDescribedByItemMove={'describedByDefinitionMove'}
+              textBtnMoveItemUp={'Move Definition up'}
+              textBtnMoveItemDown={'Move Definition down'}
+              handleClickMoveItemUp={this.handleClickMoveItemUp}
+              handleClickMoveItemDown={this.handleClickMoveItemDown}
+            />
+          </div>
+        </div>
+      </fieldset>
+    )
   }
 
   _generateHiddenInput = () => {
@@ -148,16 +188,27 @@ Definition up' and 'Move Definition down' buttons`}
       const id = element.props.id
       return itemData[id]
     })
-    return <input type="hidden" name="fv:definitions" value={JSON.stringify(arrayOfObjects)} />
+
+    return <input type="hidden" name={this.props.name} value={JSON.stringify(arrayOfObjects)} />
   }
 
   _handleChange = (arg) => {
     const { id, value, property } = arg
     const { itemData: _itemData } = this.state
     _itemData[id][property] = value
-    this.setState({
-      itemData: _itemData,
-    })
+    this.setState(
+      {
+        itemData: _itemData,
+      },
+      () => {
+        const { items, itemData } = this.state
+        const arrayOfObjects = items.map((element) => {
+          const _id = element.props.id
+          return itemData[_id]
+        })
+        this.props.handleChange(arrayOfObjects)
+      }
+    )
   }
 
   handleClickRemoveItem = (id) => {
