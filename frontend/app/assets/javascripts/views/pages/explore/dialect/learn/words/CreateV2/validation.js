e21475df7c3e8fac53930cb597f27109a1172172
Chad Oakenfold <web@oakenfold.ca>
2019-05-03 11:38:22 -0700

    Word: external validatore

---
+++ frontend/app/assets/javascripts/views/pages/explore/dialect/learn/words/CreateV2/validation.js
@@ -0,0 +1,36 @@
+// TODO: IMPORT ONLY WHAT IS USED
+import * as yup from 'yup'
+
+// import copy from './internationalization'
+
+const validForm = yup.object().shape({
+  'dc:title': yup
+    .string()
+    .label('Name') // used when errored, message will say 'Name' instead of 'dc:title'
+    .required(),
+  'fv-word:part_of_speech': yup
+    .string()
+    .label('Part of speech')
+    .required(),
+  'fv-word:pronunciation': yup.string(),
+  'fv-word:available_in_games': yup.string(),
+  'fv:available_in_childrens_archive': yup.string(),
+  'fv:cultural_note': yup.array().of(yup.string()),
+  'fv:definitions': yup.array().of(yup.object().shape({ language: yup.string(), translation: yup.string() })),
+  'fv:literal_translation': yup.array().of(yup.object().shape({ language: yup.string(), translation: yup.string() })),
+  'fv:reference': yup.string(),
+  'fv:related_audio': yup.array().of(yup.string()),
+  'fv:related_pictures': yup.array().of(yup.string()),
+  'fv:source': yup.array().of(yup.string()),
+})
+
+export const toParse = [
+  /^fv:literal_translation/,
+  /^fv:definitions/,
+  /^fv:related_audio/,
+  /^fv:related_pictures/,
+  /^fv:cultural_note/,
+  /^fvm:source/,
+  /^fv-word:related_phrases/,
+]
+export default validForm
