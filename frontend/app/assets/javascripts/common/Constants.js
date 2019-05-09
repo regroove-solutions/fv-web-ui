85b0c86b344e9778ee4dc3ca401f8684240d3970
Chad Oakenfold <web@oakenfold.ca>
2019-05-09 14:27:41 -0700

    New constants: page, size, lang, col, type

--- frontend/app/assets/javascripts/common/Constants.js
+++ frontend/app/assets/javascripts/common/Constants.js
@@ -3,3 +3,9 @@ export const STATE_DEFAULT = 1 // initial, loaded state, eg: displaying a form
 export const STATE_ERROR = 2 // component is not happy, ie: form validation (not .js errors)
 export const STATE_SUCCESS = 3 // component is happy, ie: form submitted
 export const STATE_ERROR_BOUNDARY = 4 // component has triggered a .js error or is completely messed up
+
+export const DEFAULT_PAGE = 1
+export const DEFAULT_PAGE_SIZE = 100
+export const DEFAULT_LANGUAGE = 'english'
+export const DEFAULT_SORT_COL = 'dc:title'
+export const DEFAULT_SORT_TYPE = 'asc'
