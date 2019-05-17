0cbaed79fcc726e6e9ec02d2d132e90378ec50a4
Chad Oakenfold <web@oakenfold.ca>
2019-05-16 22:32:04 -0700

    Adding new reducers to reducers/index

--- frontend/app/assets/javascripts/providers/redux/reducers/index.js
+++ frontend/app/assets/javascripts/providers/redux/reducers/index.js
@@ -1,13 +1,27 @@
 import { combineReducers } from 'redux'
+
+import { directoryReducer } from './directory'
+import { documentReducer } from './document'
 import { errorReducer } from './error'
+import { exportDialectReducer } from './exportDialect'
+import { fvAudioReducer } from './fvAudio'
+import { fvBookReducer } from './fvBook'
+import { fvCategoryReducer } from './fvCategory'
+import { fvCharacterReducer } from './fvCharacter'
 import { fvContributorReducer } from './fvContributor'
 import { fvDialectReducer } from './fvDialect'
-import { documentReducer } from './document'
 import { windowPathReducer } from './windowPath'
+
 export default combineReducers({
+  directory: directoryReducer,
+  document: documentReducer,
   error: errorReducer,
+  exportDialect: exportDialectReducer,
+  fvAudio: fvAudioReducer,
+  fvBook: fvBookReducer,
+  fvCategory: fvCategoryReducer,
+  fvCharacter: fvCharacterReducer,
   fvContributor: fvContributorReducer,
   fvDialect: fvDialectReducer,
-  document: documentReducer,
   windowPath: windowPathReducer,
 })
