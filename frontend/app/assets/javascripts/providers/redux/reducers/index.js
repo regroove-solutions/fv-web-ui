2448f8899feeb3944ae22d5774704c3e3fb5b239
Chad Oakenfold <web@oakenfold.ca>
2019-05-21 16:20:23 -0700

    + reducers to `providers/redux/reducers/index`

--- frontend/app/assets/javascripts/providers/redux/reducers/index.js
+++ frontend/app/assets/javascripts/providers/redux/reducers/index.js
@@ -10,6 +10,23 @@ import { fvCategoryReducer } from './fvCategory'
 import { fvCharacterReducer } from './fvCharacter'
 import { fvContributorReducer } from './fvContributor'
 import { fvDialectReducer } from './fvDialect'
+import { fvGalleryReducer } from './fvGallery'
+import { fvLanguageReducer } from './fvLanguage'
+import { fvLanguageFamilyReducer } from './fvLanguageFamily'
+import { fvLinkReducer } from './fvLink'
+import { fvPageReducer } from './fvPage'
+import { fvPhraseReducer } from './fvPhrase'
+import { fvPictureReducer } from './fvPicture'
+import { fvPortalReducer } from './fvPortal'
+import { fvResourcesReducer } from './fvResources'
+import { fvUserReducer } from './fvUser'
+import { fvVideoReducer } from './fvVideo'
+import { fvWordReducer } from './fvWord'
+import { navigationReducer } from './navigation'
+import { reportsReducer } from './reports'
+import { restReducer } from './rest'
+import { searchReducer } from './search'
+import { tasksReducer } from './tasks'
 import { windowPathReducer } from './windowPath'
 
 export default combineReducers({
@@ -23,5 +40,22 @@ export default combineReducers({
   fvCharacter: fvCharacterReducer,
   fvContributor: fvContributorReducer,
   fvDialect: fvDialectReducer,
+  fvGallery: fvGalleryReducer,
+  fvLanguage: fvLanguageReducer,
+  fvLanguageFamily: fvLanguageFamilyReducer,
+  fvLink: fvLinkReducer,
+  fvPage: fvPageReducer,
+  fvPhrase: fvPhraseReducer,
+  fvPicture: fvPictureReducer,
+  fvPortal: fvPortalReducer,
+  fvResources: fvResourcesReducer,
+  fvUser: fvUserReducer,
+  fvVideo: fvVideoReducer,
+  fvWord: fvWordReducer,
+  navigation: navigationReducer,
+  reports: reportsReducer,
+  rest: restReducer,
+  search: searchReducer,
+  tasks: tasksReducer,
   windowPath: windowPathReducer,
 })
