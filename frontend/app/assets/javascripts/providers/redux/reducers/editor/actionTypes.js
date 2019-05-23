8fa8b5c91aeb4b9ee76a8ea4214f9a763295e40d
Chad Oakenfold <web@oakenfold.ca>
2019-05-23 11:08:33 -0700

    Reducer: editor

---
+++ frontend/app/assets/javascripts/providers/redux/reducers/editor/actionTypes.js
@@ -0,0 +1,18 @@
+// UI
+export const DISMISS_ERROR = 'DISMISS_ERROR'
+
+// Action Constants
+export const ENABLE_EDIT_MODE = 'ENABLE_EDIT_MODE'
+export const DISABLE_EDIT_MODE = 'DISABLE_EDIT_MODE'
+
+// EDITING
+export const REQUEST_SAVE_FIELD = 'REQUEST_SAVE_FIELD'
+
+/*
+import {
+    DISMISS_ERROR,
+    ENABLE_EDIT_MODE,
+    DISABLE_EDIT_MODE,
+    REQUEST_SAVE_FIELD,
+} from './actionTypes'
+*/
