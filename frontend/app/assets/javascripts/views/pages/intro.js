5b6ed6ea0b67d0277903f85647965045fdc3f0f9
Chad Oakenfold <web@oakenfold.ca>
2019-05-28 09:08:33 -0700

    Reduxed: `.../views/pages/intro.js`

--- frontend/app/assets/javascripts/views/pages/intro.js
+++ frontend/app/assets/javascripts/views/pages/intro.js
@@ -14,40 +14,32 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import React, { Component, PropTypes } from 'react'
-import Immutable, { List } from 'immutable'
 
-import provide from 'react-redux-provide'
-import selectn from 'selectn'
+// REDUX
+import { connect } from 'react-redux'
+// REDUX: actions/dispatch/func
+import { pushWindowPath } from 'providers/redux/reducers/windowPath'
+
 import classNames from 'classnames'
-
-import ProviderHelpers from 'common/ProviderHelpers'
-
-import GridList from 'material-ui/lib/grid-list/grid-list'
-import GridTile from 'material-ui/lib/grid-list/grid-tile'
-import CircularProgress from 'material-ui/lib/circular-progress'
-import Paper from 'material-ui/lib/paper'
 import RaisedButton from 'material-ui/lib/raised-button'
-
-import TextField from 'material-ui/lib/text-field'
-
-import IconMenu from 'material-ui/lib/menus/icon-menu'
-import MenuItem from 'material-ui/lib/menus/menu-item'
 import IntlService from 'views/services/intl'
 
 /**
  * Explore Archive page shows all the families in the archive
  */
-@provide
-export default class PageHome extends Component {
+const { func, object, string } = PropTypes
+export class PageHome extends Component {
   intl = IntlService.instance
   static propTypes = {
-    properties: PropTypes.object.isRequired,
-    windowPath: PropTypes.string.isRequired,
-    pushWindowPath: PropTypes.func.isRequired,
+    // REDUX: reducers/state
+    properties: object.isRequired,
+    windowPath: string.isRequired,
+    // REDUX: actions/dispatch/func
+    pushWindowPath: func.isRequired,
   }
 
   /*static contextTypes = {
-        muiTheme: React.PropTypes.object.isRequired
+        muiTheme: React.object.isRequired
     };*/
 
   constructor(props, context) {
@@ -99,7 +91,7 @@ export default class PageHome extends Component {
             </a>
             <RaisedButton
               label={this.intl.translate({ key: 'enter_firstvoices', default: 'Enter FirstVoices' })}
-              onClick={(e, v) => this._onNavigateRequest('/')}
+              onClick={() => this._onNavigateRequest('/')}
               style={{ textAlign: 'center' }}
             />
             <p>
@@ -122,7 +114,7 @@ export default class PageHome extends Component {
 
         <div className="row">
           <div className={classNames('col-xs-12')} style={{ textAlign: 'center', padding: '15px 0' }}>
-            <a href="http://www.fpcc.ca/" target="_blank">
+            <a href="http://www.fpcc.ca/" target="_blank" rel="noopener noreferrer">
               <img
                 src="assets/images/logos/fphlcc-logo_sm.gif"
                 alt="First Peoples' Heritage Language and Culture Council "
@@ -134,7 +126,7 @@ export default class PageHome extends Component {
               />
             </a>{' '}
             &nbsp;
-            <a href="http://www.fpcf.ca/" target="_blank">
+            <a href="http://www.fpcf.ca/" target="_blank" rel="noopener noreferrer">
               <img
                 src="assets/images/logos/fpcf-logo_sm.gif"
                 alt="First Peoples' Cultural Foundation   "
@@ -146,7 +138,7 @@ export default class PageHome extends Component {
               />
             </a>
             <p style={{ padding: '15px 0' }}>We gratefully acknowledge the following supporters:</p>
-            <a href="http://www.gov.bc.ca/arr/" target="_blank">
+            <a href="http://www.gov.bc.ca/arr/" target="_blank" rel="noopener noreferrer">
               <img
                 src="assets/images/logos/BC_ARR_H.jpg"
                 alt="Ministry of Aboriginal Relations and Reconcilation"
@@ -157,7 +149,7 @@ export default class PageHome extends Component {
               />
             </a>{' '}
             &nbsp;
-            <a href="http://www.pch.gc.ca/" target="_blank">
+            <a href="http://www.pch.gc.ca/" target="_blank" rel="noopener noreferrer">
               <img
                 src="assets/images/logos/logo_pch.gif"
                 alt="Canadian Heritage"
@@ -168,7 +160,7 @@ export default class PageHome extends Component {
               />
             </a>{' '}
             &nbsp;
-            <a href="http://www.fntc.info/" target="_blank">
+            <a href="http://www.fntc.info/" target="_blank" rel="noopener noreferrer">
               <img
                 src="assets/images/logos/FNTC.gif"
                 alt="First Nation Technology Council"
@@ -178,7 +170,7 @@ export default class PageHome extends Component {
               />
             </a>{' '}
             &nbsp;
-            <a href="http://www.newrelationshiptrust.ca/" target="_blank">
+            <a href="http://www.newrelationshiptrust.ca/" target="_blank" rel="noopener noreferrer">
               <img
                 src="assets/images/logos/New-NRT-Logo-sm.jpg"
                 alt="New Relationship Trust"
@@ -189,11 +181,11 @@ export default class PageHome extends Component {
               />
             </a>{' '}
             &nbsp;
-            <a href="http://www.languagegeek.com" target="_blank">
+            <a href="http://www.languagegeek.com" target="_blank" rel="noopener noreferrer">
               <img src="assets/images/logos/lg.gif" alt="Languagegeek.com" width="191" height="36" border="0" />
             </a>{' '}
             &nbsp;
-            <a href="http://www.tavultesoft.com/" target="_blank">
+            <a href="http://www.tavultesoft.com/" target="_blank" rel="noopener noreferrer">
               <img src="assets/images/logos/tav.gif" alt="Tavultesoft" width="191" height="29" border="0" />
             </a>
           </div>
@@ -204,3 +196,26 @@ export default class PageHome extends Component {
     )
   }
 }
+
+// REDUX: reducers/state
+const mapStateToProps = (state /*, ownProps*/) => {
+  const { navigation, windowPath } = state
+
+  const { properties } = navigation
+  const { _windowPath } = windowPath
+
+  return {
+    properties,
+    windowPath: _windowPath,
+  }
+}
+
+// REDUX: actions/dispatch/func
+const mapDispatchToProps = {
+  pushWindowPath,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PageHome)
