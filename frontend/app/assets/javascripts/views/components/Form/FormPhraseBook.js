febf7d9bb3f8833b8a4758bb32ebc39d8045a302
Chad Oakenfold <web@oakenfold.ca>
2019-06-04 08:59:40 -0700

    Reduxed: FormPhraseBook

--- frontend/app/assets/javascripts/views/components/Form/FormPhraseBook.js
+++ frontend/app/assets/javascripts/views/components/Form/FormPhraseBook.js
@@ -6,8 +6,11 @@ import FormRemoveButton from 'views/components/Form/FormRemoveButton'
 // import ProviderHelpers from 'common/ProviderHelpers'
 import Preview from 'views/components/Editor/Preview'
 
-// see about dropping:
-import provide from 'react-redux-provide'
+// REDUX
+import { connect } from 'react-redux'
+// REDUX: actions/dispatch/func
+import { createCategory } from 'providers/redux/reducers/fvCategory'
+
 import ProviderHelpers from 'common/ProviderHelpers'
 
 const { array, func, object, number, string, element } = PropTypes
@@ -41,12 +44,13 @@ export class FormPhraseBook extends React.Component {
     componentState: number,
     value: string,
     browseComponent: element.isRequired,
-    // NOTE: COMING FROM REDUX/PROVIDER
-    computeCategory: object.isRequired,
-    createCategory: func.isRequired,
-    splitWindowPath: array.isRequired,
     // NOTE: COMING FROM PARENT COMPONENT, NOT REDUX/PROVIDER
     computeDialectFromParent: object.isRequired,
+    // REDUX: reducers/state
+    computeCategory: object.isRequired,
+    splitWindowPath: array.isRequired,
+    // REDUX: actions/dispatch/func‘
+    createCategory: func.isRequired,
   }
   static defaultProps = {
     className: 'FormPhraseBook',
@@ -146,4 +150,25 @@ export class FormPhraseBook extends React.Component {
   }
 }
 
-export default provide(FormPhraseBook)
+// REDUX: reducers/state
+const mapStateToProps = (state /*, ownProps*/) => {
+  const { fvCategory, windowPath } = state
+
+  const { computeCategory } = fvCategory
+  const { splitWindowPath } = windowPath
+
+  return {
+    computeCategory,
+    splitWindowPath,
+  }
+}
+
+// REDUX: actions/dispatch/func
+const mapDispatchToProps = {
+  createCategory,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormPhraseBook)
