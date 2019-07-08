c1e6b83bcbacfc08f7992c3e13b052ca8e77e76d
Chad Oakenfold <web@oakenfold.ca>
2019-07-08 09:25:36 -0700

    New Spinner Component: 3 balls falling

---
+++ frontend/app/assets/javascripts/views/components/SpinnerBallFall/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import '!style-loader!css-loader!./SpinnerBallFall.css'
+import { PropTypes } from 'react'
+const { string, number } = PropTypes
+export class SpinnerBallFall extends React.Component {
+  static propTypes = {
+    className: string,
+    variation: number,
+  }
+  static defaultProps = {
+    className: '',
+    variation: 1,
+  }
+  render() {
+    let classNameModifier = ''
+    switch (this.props.variation) {
+      case 1:
+        classNameModifier = 'la-sm'
+        break
+      case 2:
+        classNameModifier = 'la-dark'
+        break
+      case 3:
+        classNameModifier = 'la-2x'
+        break
+      case 4:
+        classNameModifier = 'la-3x'
+        break
+      default: // Note: do nothing
+    }
+    return (
+      <div className={`la-ball-fall la-dark ${classNameModifier} ${this.props.className}`}>
+        <div />
+        <div />
+        <div />
+      </div>
+    )
+  }
+}
+
+export default SpinnerBallFall
