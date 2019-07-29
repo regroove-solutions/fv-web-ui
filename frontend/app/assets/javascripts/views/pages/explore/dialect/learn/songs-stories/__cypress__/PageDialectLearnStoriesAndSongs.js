3e6be9c9a8a6a9e815d43bc85cf70ba677cb2b58
Chad Oakenfold <web@oakenfold.ca>
2019-07-29 12:58:00 -0700

    FW-257: Cypress tests

---
+++ frontend/app/assets/javascripts/views/pages/explore/dialect/learn/songs-stories/__cypress__/PageDialectLearnStoriesAndSongs.js
@@ -0,0 +1,34 @@
+import 'cypress-testing-library/add-commands'
+describe('PageDialectLearnStoriesAndSongs', () => {
+  it('FW-257: On the Browse > Songs page, the user can click a button to get to Create Song page', () => {
+    cy.log('NOTE: Test expects to be run with `npm run startPreprod`')
+    cy.login({
+      userName: 'SENCOTEN_USERNAME',
+      userPassword: 'SENCOTEN_PASSWORD',
+    })
+    cy.visit(
+      'http://0.0.0.0:3001/nuxeo/app/explore/FV/Workspaces/Data/THE%20SEN%C4%86O%C5%A6EN%20LANGUAGE/SEN%C4%86O%C5%A6EN/SEN%C4%86O%C5%A6EN/learn/songs'
+    )
+
+    cy.getByText('create song book', { exact: false }).click()
+    cy.getByText('Add new song book to', { exact: false }).should('exist')
+
+    cy.log('Test complete')
+  })
+
+  it('FW-257: On the Browse > Stories page, the user can click a button to get to Create Story page', () => {
+    cy.log('NOTE: Test expects to be run with `npm run startPreprod`')
+    cy.login({
+      userName: 'SENCOTEN_USERNAME',
+      userPassword: 'SENCOTEN_PASSWORD',
+    })
+    cy.visit(
+      'http://0.0.0.0:3001/nuxeo/app/explore/FV/Workspaces/Data/THE%20SEN%C4%86O%C5%A6EN%20LANGUAGE/SEN%C4%86O%C5%A6EN/SEN%C4%86O%C5%A6EN/learn/stories'
+    )
+
+    cy.getByText('create story book', { exact: false }).click()
+    cy.getByText('Add new story book to', { exact: false }).should('exist')
+
+    cy.log('Test complete')
+  })
+})
