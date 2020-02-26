74b4657b9c04a766b9be7f1637cea4324762fc2f
Liam <liam.workman@gmail.com>
2020-02-26 13:39:38 -0800

    Started work on retrieving audio for translation

--- frontend/app/assets/javascripts/views/components/FVLabel/index.js
+++ frontend/app/assets/javascripts/views/components/FVLabel/index.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import selectn from 'selectn'
 import proptypes from 'prop-types'
 import { connect } from 'react-redux'
 import Menu from '@material-ui/core/Menu'
 import ListItem from '@material-ui/core/ListItem'
+import DocumentOperations from 'operations/DocumentOperations'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import '!style-loader!css-loader!./FVLabel.css'
 
 function FVLabel({ transKey, defaultStr, transform, params, prepend, append, forceLocale, intl, locale, isInHelpMode }) {
   const [anchorElement, setAnchorElement] = useState()
+  const [audioId, setAudioId] = useState('')
+  const [isFetchingAudio, setIsFetchingAudio] = useState('')
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+
+    return () => {
+      setIsMounted(false)
+    }
+  }, [])
 
   const handleClick = (event) => {
     if (isInHelpMode) {
@@ -16,6 +30,15 @@ function FVLabel({ transKey, defaultStr, transform, params, prepend, append, for
         setAnchorElement(undefined)
       } else {
         setAnchorElement(event.currentTarget)
+        setIsFetchingAudio(true)
+        DocumentOperations.getDocument('d7ec7c47-47d7-495f-a08e-6c8ba7863c93', 'FVLabel').then(data => {
+          if (isMounted) {
+            setAudioId(selectn('properties.fv:related_audio[0]', data))
+            setIsFetchingAudio(false)
+          } else {
+            console.log("Ahh I'm not mounted!")
+          }
+        })
       }
       console.log(transKey)
     }
@@ -40,9 +63,11 @@ function FVLabel({ transKey, defaultStr, transform, params, prepend, append, for
       {intl.trans(transKey, defaultStr, transform, params, prepend, append, forceLocale)}
       {isInHelpMode && (
         <span onClick={handleClick} className="fv-label-click-cover">
-          <Menu id="simple-menu" anchorEl={anchorElement} open={!!anchorElement} onClose={handleClose} getContentAnchorEl={null} anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}>
+          <Menu id="simple-menu" anchorEl={anchorElement} open={!!anchorElement} onClose={handleClose} getContentAnchorEl={null} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
             <ListItem>Translation: {intl.trans(transKey, defaultStr, transform, params, prepend, append, locale)}</ListItem>
-            <ListItem button onClick={playAudio}>Play Audio</ListItem>
+            {!isFetchingAudio && audioId && <ListItem button onClick={playAudio}>Play Audio</ListItem>}
+            {!isFetchingAudio && !audioId && <ListItem button disabled>No audio</ListItem>}
+            {isFetchingAudio && <ListItem><CircularProgress size={21} style={{ margin: '0 auto' }} /></ListItem>}
             <ListItem button onClick={openEdit}>Edit</ListItem>
           </Menu>
         </span>
