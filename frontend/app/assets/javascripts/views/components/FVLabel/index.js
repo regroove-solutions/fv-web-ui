0f95fe0e9f665563d9518cd4512f442ddd5dd590
Liam <liam.workman@gmail.com>
2020-02-26 11:50:38 -0800

    First version of helper mode menu

--- frontend/app/assets/javascripts/views/components/FVLabel/index.js
+++ frontend/app/assets/javascripts/views/components/FVLabel/index.js
@@ -1,24 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import proptypes from 'prop-types'
 import { connect } from 'react-redux'
+import Menu from '@material-ui/core/Menu'
+import ListItem from '@material-ui/core/ListItem'
+import '!style-loader!css-loader!./FVLabel.css'
 
-function FVLabel({
-  transKey,
-  defaultStr,
-  transform,
-  params,
-  prepend,
-  append,
-  forceLocale,
-  intl,
-  locale,
-  showTwoLanguages,
-}) {
+function FVLabel({ transKey, defaultStr, transform, params, prepend, append, forceLocale, intl, locale }) {
+  const isInHelpMode = true
+  const [anchorElement, setAnchorElement] = useState()
+
+  const handleClick = (event) => {
+    if (isInHelpMode) {
+      event.preventDefault()
+      event.stopPropagation()
+      if (anchorElement) {
+        setAnchorElement(undefined)
+      } else {
+        setAnchorElement(event.currentTarget)
+      }
+      console.log(transKey)
+    }
+  }
+
+  const handleClose = () => {
+    setAnchorElement(undefined)
+  }
+
+  const playAudio = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+
+  const openEdit = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+
   return (
-    <>
+    <span className="fv-label">
       {intl.trans(transKey, defaultStr, transform, params, prepend, append, forceLocale)}
-      {showTwoLanguages && <>{intl.trans(transKey, defaultStr, transform, params, prepend, append, locale)}</>}
-    </>
+      {isInHelpMode && (
+        <span onClick={handleClick} className="fv-label-click-cover">
+          <Menu id="simple-menu" anchorEl={anchorElement} open={!!anchorElement} onClose={handleClose}  anchorOrigin={{vertical: 'top', horizontal: 'right'}}>
+            <ListItem>Translation: {intl.trans(transKey, defaultStr, transform, params, prepend, append, locale)}</ListItem>
+            <ListItem button onClick={playAudio}>Play Audio</ListItem>
+            <ListItem button onClick={openEdit}>Edit</ListItem>
+          </Menu>
+        </span>
+      )}
+    </span>
   )
+}
+
+const { string, array, object } = proptypes
+
+FVLabel.propTypes = {
+  transKey: string.isRequired,
+  defaultStr: string,
+  transform: string,
+  params: array,
+  prepend: string,
+  append: string,
+  forceLocale: string,
+  locale: string,
+  intl: object.isRequired,
 }
 
 const mapStateToProps = (state /*, ownProps*/) => {
@@ -26,7 +71,6 @@ const mapStateToProps = (state /*, ownProps*/) => {
 
   return {
     intl: locale.intlService,
-    showTwoLanguages: locale.immersionMode === 2,
     locale: locale.locale,
   }
 }
